ed34e396b4277c434a0c39a2810b377930478e90
pytzcarraldo <alexander.schaaf@rwth-aachen.de>
2017-08-18 17:06:25 +0200

    - Posterior class enhancements

--- gempy/UncertaintyAnalysisPYMC2.py
+++ gempy/UncertaintyAnalysisPYMC2.py
@@ -5,7 +5,9 @@ import pymc
 import theano
 import numpy as np
 import networkx as nx
-
+import gempy as gp
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Posterior:
     """Posterior database analysis for GemPy-pymc2 hdf5 databases."""
@@ -20,16 +22,21 @@ class Posterior:
         try:
             self.sols = self.db.gempy_model.gettrace()
         except AttributeError:
-            print("No GemPy models tallied.")
+            print("No GemPy model trace tallied.")
             self.sols = None
 
-        if topology:
+        try:
             # load graphs
             topo_trace = self.db.gempy_topo.gettrace()
-            self.graphs = topo_trace[:, 0]
+            self.topo_graphs = topo_trace[:, 0]
             # load centroids
-            self.centroids = topo_trace[:, 1]
+            self.topo_centroids = topo_trace[:, 1]
+            self.topo_labels_unique = topo_trace[:, 2]
+            self.topo_lith_to_labels_lot = topo_trace[:, 3]
+            self.topo_labels_to_lith_lot = topo_trace[:, 4]
             del topo_trace
+        except AttributeError:
+            print("No Topology trace tallied.")
 
         # load input data
         self.input_data = self.db.input_data.gettrace()
@@ -52,7 +59,7 @@ class Posterior:
 
     def plot_topology_graph(self, i):
         # get centroid values into list
-        centroid_values = [triplet for triplet in self.centroids[i].values()]
+        centroid_values = [triplet for triplet in self.topo_centroids[i].values()]
         # unzip them into seperate lists of x,y,z coordinates
         centroids_x, centroids_y, centroids_z = list(zip(*centroid_values))
         # create new 2d pos dict for plot
@@ -60,6 +67,99 @@ class Posterior:
         for j in range(len(centroids_x)):  # TODO: Change this directly to use zip?
             pos_dict[j + 1] = [centroids_x[j], centroids_y[j]]
         # draw
-        nx.draw_networkx(self.graphs[i], pos=pos_dict)
+        nx.draw_networkx(self.topo_graphs[i], pos=pos_dict)
+
+    def plot_section(self, interp_data, i, dim, plot_data=False, plot_topo=False):
+        self.change_input_data(interp_data, i)
+        lith_block, fault_block = gp.compute_model(interp_data)
+        plt.imshow(lith_block[-1, 0,:].reshape(dim[0], dim[1], dim[2])[:, 0, :].T, origin="lower",
+                   cmap=gp.colors.cmap, norm=gp.colors.norm)
+        #gp.plot_section(interp_data.geo_data_res, lith_block[0, 0, :], 0)
+
+        rs = interp_data.rescaling_factor
+        #if plot_data:
+        #    plt.scatter(interp_data.geo_data_res.interfaces["X"].values,
+        #                interp_data.geo_data_res.interfaces["Z"].values)
+
+        if plot_topo:
+            self.topo_plot_graph(i)
+
+    def topo_plot_graph(self, i):
+        pos_2d = {}
+        for key in self.topo_centroids[i].keys():
+            pos_2d[key] = [self.topo_centroids[i][key][0], self.topo_centroids[i][key][2]]
+        nx.draw_networkx(self.topo_graphs[i], pos=pos_2d)
+
+
+def compare_graphs(G1, G2):
+    intersection = 0
+    union = G1.number_of_edges()
+
+    for edge in G1.edges_iter():
+        if G2.has_edge(edge[0], edge[1]):
+            intersection += 1
+        else:
+            union += 1
+
+    return intersection / union
+
+
+class Plane:
+    def __init__(self, group_id, geo_data):
+        self.group_id = group_id
+        self.geo_data = geo_data
+
+        # create dataframe bool filters for convenience
+        self.fol_f = self.geo_data.foliations["group_id"] == self.group_id
+        self.interf_f = self.geo_data.interfaces["group_id"] == self.group_id
+
+        # get indices for both foliations and interfaces
+        self.interf_i = self.geo_data.interfaces[self.interf_f].index
+        self.fol_i = self.geo_data.foliations[self.fol_f].index[0]
+
+        # normal
+        self.normal = self.get_normal()
+        # centroid
+        self.centroid = [float(self.geo_data.foliations[self.fol_f]["X"]),
+                         float(self.geo_data.foliations[self.fol_f]["Y"]),
+                         float(self.geo_data.foliations[self.fol_f]["Z"])]
+
+    # method: give dip, change interfaces accordingly
+    def interf_recalc(self, dip):
+        """Changes the dip of plane and recalculates Z coordinates for the points belonging to it."""
+        # modify the foliation
+        self.geo_data.foliations.set_value(self.fol_i, "dip", dip)
+        # get azimuth
+        az = float(self.geo_data.foliations.iloc[self.fol_i]["azimuth"])
+        # set polarity according to dip
+        if -90 < dip < 90:
+            polarity = 1
+        else:
+            polarity = -1
+        self.geo_data.foliations.set_value(self.fol_i, "polarity", polarity)
+        # modify gradient
+        self.geo_data.foliations.set_value(self.fol_i, "G_x",
+                                           np.sin(np.deg2rad(dip)) * np.sin(np.deg2rad(az)) * polarity)
+        self.geo_data.foliations.set_value(self.fol_i, "G_y",
+                                           np.sin(np.deg2rad(dip)) * np.cos(np.deg2rad(az)) * polarity)
+        self.geo_data.foliations.set_value(self.fol_i, "G_z", np.cos(np.deg2rad(dip)) * polarity)
+
+        # update normal
+        self.normal = self.get_normal()
+        # modify points (Z only so far)
+        a, b, c = self.normal
+        d = -a * self.centroid[0] - b * self.centroid[1] - c * self.centroid[2]
+        for i, row in self.geo_data.interfaces[self.interf_f].iterrows():
+            # iterate over each point and recalculate Z, set Z
+            # x, y, z = row["X"], row["Y"], row["Z"]
+            Z = (a * row["X"] + b * row["Y"] + d) / -c
+            self.geo_data.interfaces.set_value(i, "Z", Z)
+
+    def get_normal(self):
+        """Just returns updated normal vector (values from dataframe)."""
+        normal = [float(self.geo_data.foliations.iloc[self.fol_i]["G_x"]),
+                  float(self.geo_data.foliations.iloc[self.fol_i]["G_y"]),
+                  float(self.geo_data.foliations.iloc[self.fol_i]["G_z"])]
+        return normal
 
 
