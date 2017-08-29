2a31c06abaf796f756699fb160d043509d52c665
pytzcarraldo <alexander.schaaf@rwth-aachen.de>
2017-08-29 16:36:18 +0200

    - modified posterior section plot to be more modular - added tutorial ch3 input data

--- gempy/UncertaintyAnalysisPYMC2.py
+++ gempy/UncertaintyAnalysisPYMC2.py
@@ -69,12 +69,16 @@ class Posterior:
         # draw
         nx.draw_networkx(self.topo_graphs[i], pos=pos_dict)
 
+    def compute_posterior_model(self, interp_data, i):
+        self.change_input_data(interp_data, i)
+        return gp.compute_model(interp_data)
+
     def plot_section(self, interp_data, i, dim, plot_data=False, plot_topo=False):
         self.change_input_data(interp_data, i)
         lith_block, fault_block = gp.compute_model(interp_data)
         #plt.imshow(lith_block[-1, 0,:].reshape(dim[0], dim[1], dim[2])[:, 0, :].T, origin="lower",
         #           cmap=gp.colors.cmap, norm=gp.colors.norm)
-        gp.plot_section(interp_data.geo_data_res, lith_block[0, 0, :], 0, plot_data=plot_data)
+        gp.plot_section(interp_data.geo_data_res, lith_block[0], 0, plot_data=plot_data)
 
         rs = interp_data.rescaling_factor
         #if plot_data:
@@ -105,61 +109,68 @@ def compare_graphs(G1, G2):
 
 
 class Plane:
-    def __init__(self, group_id, geo_data):
+    def __init__(self, group_id, data_obj):
         self.group_id = group_id
-        self.geo_data = geo_data
+        self.data_obj = data_obj
 
         # create dataframe bool filters for convenience
-        self.fol_f = self.geo_data.foliations["group_id"] == self.group_id
-        self.interf_f = self.geo_data.interfaces["group_id"] == self.group_id
+        self.fol_f = self.data_obj.foliations["group_id"] == self.group_id
+        self.interf_f = self.data_obj.interfaces["group_id"] == self.group_id
 
         # get indices for both foliations and interfaces
-        self.interf_i = self.geo_data.interfaces[self.interf_f].index
-        self.fol_i = self.geo_data.foliations[self.fol_f].index[0]
+        self.interf_i = self.data_obj.interfaces[self.interf_f].index
+        self.fol_i = self.data_obj.foliations[self.fol_f].index[0]
 
         # normal
-        self.normal = self.get_normal()
+        self.normal = None
         # centroid
-        self.centroid = [float(self.geo_data.foliations[self.fol_f]["X"]),
-                         float(self.geo_data.foliations[self.fol_f]["Y"]),
-                         float(self.geo_data.foliations[self.fol_f]["Z"])]
+        self.centroid = None
+        self.refresh()
 
     # method: give dip, change interfaces accordingly
     def interf_recalc(self, dip):
         """Changes the dip of plane and recalculates Z coordinates for the points belonging to it."""
         # modify the foliation
-        self.geo_data.foliations.set_value(self.fol_i, "dip", dip)
+        self.data_obj.foliations.set_value(self.fol_i, "dip", dip)
         # get azimuth
-        az = float(self.geo_data.foliations.iloc[self.fol_i]["azimuth"])
+        az = float(self.data_obj.foliations.iloc[self.fol_i]["azimuth"])
         # set polarity according to dip
         if -90 < dip < 90:
             polarity = 1
         else:
             polarity = -1
-        self.geo_data.foliations.set_value(self.fol_i, "polarity", polarity)
+        self.data_obj.foliations.set_value(self.fol_i, "polarity", polarity)
         # modify gradient
-        self.geo_data.foliations.set_value(self.fol_i, "G_x",
+        self.data_obj.foliations.set_value(self.fol_i, "G_x",
                                            np.sin(np.deg2rad(dip)) * np.sin(np.deg2rad(az)) * polarity)
-        self.geo_data.foliations.set_value(self.fol_i, "G_y",
+        self.data_obj.foliations.set_value(self.fol_i, "G_y",
                                            np.sin(np.deg2rad(dip)) * np.cos(np.deg2rad(az)) * polarity)
-        self.geo_data.foliations.set_value(self.fol_i, "G_z", np.cos(np.deg2rad(dip)) * polarity)
+        self.data_obj.foliations.set_value(self.fol_i, "G_z", np.cos(np.deg2rad(dip)) * polarity)
 
         # update normal
         self.normal = self.get_normal()
         # modify points (Z only so far)
         a, b, c = self.normal
         d = -a * self.centroid[0] - b * self.centroid[1] - c * self.centroid[2]
-        for i, row in self.geo_data.interfaces[self.interf_f].iterrows():
+        for i, row in self.data_obj.interfaces[self.interf_f].iterrows():
             # iterate over each point and recalculate Z, set Z
             # x, y, z = row["X"], row["Y"], row["Z"]
             Z = (a * row["X"] + b * row["Y"] + d) / -c
-            self.geo_data.interfaces.set_value(i, "Z", Z)
+            self.data_obj.interfaces.set_value(i, "Z", Z)
+
+    def refresh(self):
+        # normal
+        self.normal = self.get_normal()
+        # centroid
+        self.centroid = [float(self.data_obj.foliations[self.fol_f]["X"]),
+                         float(self.data_obj.foliations[self.fol_f]["Y"]),
+                         float(self.data_obj.foliations[self.fol_f]["Z"])]
 
     def get_normal(self):
         """Just returns updated normal vector (values from dataframe)."""
-        normal = [float(self.geo_data.foliations.iloc[self.fol_i]["G_x"]),
-                  float(self.geo_data.foliations.iloc[self.fol_i]["G_y"]),
-                  float(self.geo_data.foliations.iloc[self.fol_i]["G_z"])]
+        normal = [float(self.data_obj.foliations.iloc[self.fol_i]["G_x"]),
+                  float(self.data_obj.foliations.iloc[self.fol_i]["G_y"]),
+                  float(self.data_obj.foliations.iloc[self.fol_i]["G_z"])]
         return normal
 
 
