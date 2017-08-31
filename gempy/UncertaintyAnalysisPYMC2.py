164b25a1957c691c70b769bcccad715b0070839a
pytzcarraldo <alexander.schaaf@rwth-aachen.de>
2017-08-31 11:01:27 +0200

    - added dip, az and polarity to get_data - fully implemented information entropy calculation

--- gempy/UncertaintyAnalysisPYMC2.py
+++ gempy/UncertaintyAnalysisPYMC2.py
@@ -9,6 +9,7 @@ import gempy as gp
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 class Posterior:
     """Posterior database analysis for GemPy-pymc2 hdf5 databases."""
 
@@ -20,12 +21,13 @@ class Posterior:
         self.trace_names = self.db.trace_names[0]
         # get gempy block models
         try:
-            self.sols = self.db.gempy_model.gettrace()
+            self.lb, self.fb = self.db.gempy_model.gettrace()
         except AttributeError:
             print("No GemPy model trace tallied.")
-            self.sols = None
+            self.lb = None
+            self.fb = None
 
-        try:
+        if topology:
             # load graphs
             topo_trace = self.db.gempy_topo.gettrace()
             self.topo_graphs = topo_trace[:, 0]
@@ -35,11 +37,13 @@ class Posterior:
             self.topo_lith_to_labels_lot = topo_trace[:, 3]
             self.topo_labels_to_lith_lot = topo_trace[:, 4]
             del topo_trace
-        except AttributeError:
-            print("No Topology trace tallied.")
 
         # load input data
         self.input_data = self.db.input_data.gettrace()
+
+        self.lith_prob = None
+        self.ie = None
+        self.ie_total = None
 
     def change_input_data(self, interp_data, i):
         """Changes input data in interp_data to posterior input data at iteration i."""
@@ -74,6 +78,7 @@ class Posterior:
         return gp.compute_model(interp_data)
 
     def plot_section(self, interp_data, i, dim, plot_data=False, plot_topo=False):
+        """Deprecated."""
         self.change_input_data(interp_data, i)
         lith_block, fault_block = gp.compute_model(interp_data)
         #plt.imshow(lith_block[-1, 0,:].reshape(dim[0], dim[1], dim[2])[:, 0, :].T, origin="lower",
@@ -93,6 +98,69 @@ class Posterior:
         for key in self.topo_centroids[i].keys():
             pos_2d[key] = [self.topo_centroids[i][key][0], self.topo_centroids[i][key][2]]
         nx.draw_networkx(self.topo_graphs[i], pos=pos_2d)
+
+    def compute_posterior_models_all(self, interp_data, n=None, calc_fb=True):
+        """Computes block models from stored input parameters for all iterations."""
+        if self.lb is None:
+            # create the storage array
+            lb, fb = self.compute_posterior_model(interp_data, 1)
+            lb = lb[0]
+            fb = fb[0]
+            self.lb = np.empty_like(lb)
+            if calc_fb:
+                self.fb = np.empty_like(lb)
+
+            # compute model for every iteration
+            if n is None:
+                n = self.db.getstate()["sampler"]["_iter"]
+            for i in range(n):
+                if i == 0:
+                    lb, fb = self.compute_posterior_model(interp_data, i)
+                    self.lb = lb[0]
+                    self.fb = fb[0]
+                else:
+                    lb, fb = self.compute_posterior_model(interp_data, i)
+                    self.lb = np.vstack((self.lb, lb[0]))
+                    if calc_fb:
+                        self.fb = np.vstack((self.fb, fb[0]))
+        else:
+            print("self.lb already filled with something.")
+
+    def compute_entropy(self, interp_data):
+        """Computes the voxel information entropy of stored block models."""
+        if self.lb is None:
+            return "No models stored in self.lb, please run 'self.compute_posterior_models_all' to generate block" \
+                   " models for all iterations."
+
+        self.lith_prob = compute_prob_lith(self.lb)
+        self.ie = calcualte_ie_masked(self.lith_prob)
+        self.ie_total = calculate_ie_total(self.ie)
+        print("Information Entropy successfully calculated. Stored in self.ie and self.ie_total")
+
+
+def compute_prob_lith(lith_blocks):
+    """Blocks must be just the lith blocks!"""
+    lith_id = np.unique(lith_blocks)
+    lith_count = np.zeros_like(lith_blocks[0:len(lith_id)])
+    for i, l_id in enumerate(lith_id):
+        lith_count[i] = np.sum(lith_blocks == l_id, axis=0)
+    lith_prob = lith_count / len(lith_blocks)
+    return lith_prob
+
+
+def calcualte_ie_masked(lith_prob):
+    ie = np.zeros_like(lith_prob[0])
+    for l in lith_prob:
+        pm = np.ma.masked_equal(l, 0)  # mask where layer prob is 0
+        ie -= (pm * np.ma.log2(pm)).filled(0)
+    return ie
+
+
+def calculate_ie_total(ie, absolute=False):
+    if absolute:
+        return np.sum(ie)
+    else:
+        return np.sum(ie) / np.size(ie)
 
 
 def compare_graphs(G1, G2):
