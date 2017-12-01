6f5f0d15cc64d6a515598574984bb1b9c16ad6fd
miguel <miguel.de.la.varga@aices.rwth-aachen.de>
2017-12-01 17:04:58 +0100

    - Few changes to the uncertainty analysis package - Set sandstone inference notebook

--- gempy/UncertaintyAnalysisPYMC2.py
+++ gempy/UncertaintyAnalysisPYMC2.py
@@ -95,7 +95,7 @@ class Posterior:
             # create the storage array
             lb, fb = self.compute_posterior_model(interp_data, 1)
             lb = lb[0]
-            fb = fb[0]
+
             self.lb = np.empty_like(lb)
             if calc_fb:
                 self.fb = np.empty_like(lb)
@@ -109,7 +109,7 @@ class Posterior:
                 if i == 0:
                     lb, fb = self.compute_posterior_model(interp_data, i)
                     self.lb = lb[0]
-                    self.fb = fb[0]
+                    self.fb = fb
                 else:
                     lb, fb = self.compute_posterior_model(interp_data, i)
                     self.lb = np.vstack((self.lb, lb[0]))
