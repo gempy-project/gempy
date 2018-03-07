211cff4180d81c9c88b30378dc34a26f30b55df4
Miguel de la Varga <varga@aices.rwth-aachen.de>
2018-03-07 14:20:18 +0100

    - prepare rc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gempy',
-    version='0.9991',
+    version='1.01',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         'numpy',
@@ -13,7 +13,7 @@ setup(
         'seaborn'
     ],
     url='https://github.com/cgre-aachen/gempy',
-    download_url='https://github.com/cgre-aachen/gempy/archive/0.9991.tar.gz',
+    download_url='https://github.com/cgre-aachen/gempy/archive/1.01.tar.gz',
     license='LGPL v3',
     author='Miguel de la Varga, Alexander Schaaf, Fabian Stamm, Florian Wellmann',
     author_email='varga@aices.rwth-aachen.de',
