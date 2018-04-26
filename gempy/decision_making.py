5dd1efafbdcd9db0393d966c0e411a5d86793491
fastamo <f.a.stamm@googlemail.com>
2018-04-26 15:26:08 +0200

    DM module continued (loss_abs and loss_sqr)

--- gempy/decision_making.py
+++ gempy/decision_making.py
@@ -24,7 +24,7 @@ def loss_abs(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
     """Absolute-error loss function.
 
         Args:
-            estimate_s (int, float or np.array): Score estimate.
+            estimate_s (int or float): Score estimate.
             true_s (int, float or np.array): True score value.
             u (int or float, optional): Underestimation re-weighting factor.
             o (int or float, optional): Overestimation re-weighting factor.
@@ -36,7 +36,14 @@ def loss_abs(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
             Based on absolute-error, i.e. the absolute distance of the estimate
             from the true value.
 
+            True_s can be either a single value or an array of possible true values,
+            the output will be a single determined value or an array of determined
+            values, accordingly.
+
+            Estimate_s has to be one single value.
+
     """
+    true_s = np.array(true_s).astype(float)
     loss_s = np.zeros_like(true_s)
     underest = (estimate_s < true_s)
     underest_fatal = (estimate_s == 0) & (true_s > 0)
@@ -48,7 +55,55 @@ def loss_abs(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
     loss_s[overest_fatal] = np.abs((true_s[overest_fatal] - estimate_s)) * (o_f * r)
     return loss_s
 
-def expected_loss(function='absolute', estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
+def loss_sqr(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
+    """Squared-error loss function.
+
+        Args:
+            estimate_s (int or float): Score estimate.
+            true_s (int, float or np.array): True score value.
+            u (int or float, optional): Underestimation re-weighting factor.
+            o (int or float, optional): Overestimation re-weighting factor.
+            u_f (int or float, optional): Fatal underestimation re-weighting factor.
+            o_f (int or float, optional): Fatal overestimation re-weighting factor.
+            r (int, float or np.array, optional): Risk-affinity re-weighting factor.
+        Returns:
+            Loss incurred for an estimate of the score given a true score.
+            Based on absolute-error, i.e. the absolute distance of the estimate
+            from the true value.
+
+            True_s can be either a single value or an array of possible true values,
+            the output will be a single determined value or an array of determined
+            values, accordingly.
+
+            Estimate_s has to be one single value.
+
+    """
+    true_s = np.array(true_s).astype(float)
+    loss_s = np.zeros_like(true_s)
+    underest = (estimate_s < true_s)
+    underest_fatal = (estimate_s == 0) & (true_s > 0)
+    overest = (estimate_s > true_s)
+    overest_fatal = (estimate_s > 0) & (true_s == 0)
+    loss_s[underest] = (true_s[underest] - estimate_s)  * (u * r)
+    loss_s[underest_fatal] = (true_s[underest_fatal] - estimate_s) * (u_f * (r ** -0.5))
+    loss_s[overest] = (estimate_s - true_s[overest]) * (o * r)
+    loss_s[overest_fatal] = np.abs((true_s[overest_fatal] - estimate_s)) * (o_f * r)
+    return np.square(loss_s)
+
+def expected_loss(estimate_s, true_s, function='absolute', u=1,o=1,u_f=1,o_f=1, r=1):
+    """Function to attain expected loss, based on an absolute-error or
+    squared error-loss function.
+
+    :param estimate_s:
+    :param true_s:
+    :param function:
+    :param u:
+    :param o:
+    :param u_f:
+    :param o_f:
+    :param r:
+    :return:
+    """
     if function == 'absolute':
         return loss_abs(estimate_s, true_s, u,o,u_f,o_f, r).mean()
     elif function == 'squared':
@@ -57,4 +112,8 @@ def expected_loss(function='absolute', estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, 
         print('Error: Type of loss function not recognized. '
               'Use "absolute" or "squared".)
 
-#def sqr_loss()
+def expected_loss_range(estimate_range, true_s, risk_range=np.array(1), function='absolute', u=1,o=1,u_f=1,o_f=1):
+    expected_loss_s = lambda estimate_s, r: expected_loss(estimate_s, true_s, function, u,o,u_f,o_f,r)
+
+    for r in risk_range:
+
