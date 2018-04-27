b89039918fa506239ea7d30acaa3be5e4a75cb08
fastamo <f.a.stamm@googlemail.com>
2018-04-27 16:16:40 +0200

    Realization of expected loss for estimate ranges and plotting for several risk_factors now working.

--- gempy/decision_making.py
+++ gempy/decision_making.py
@@ -19,6 +19,8 @@
 """
 
 import numpy as np
+import scipy.optimize as sop
+from matplotlib import pyplot as plt
 
 def loss_abs(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
     """Absolute-error loss function.
@@ -46,13 +48,15 @@ def loss_abs(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
     true_s = np.array(true_s).astype(float)
     loss_s = np.zeros_like(true_s)
     underest = (estimate_s < true_s)
-    underest_fatal = (estimate_s == 0) & (true_s > 0)
     overest = (estimate_s > true_s)
-    overest_fatal = (estimate_s > 0) & (true_s == 0)
-    loss_s[underest] = (true_s[underest] - estimate_s)  * (u * r)
-    loss_s[underest_fatal] = (true_s[underest_fatal] - estimate_s) * (u_f * (r ** -0.5))
+    loss_s[underest] = (true_s[underest] - estimate_s)  * (u * (r ** -0.5))
     loss_s[overest] = (estimate_s - true_s[overest]) * (o * r)
-    loss_s[overest_fatal] = np.abs((true_s[overest_fatal] - estimate_s)) * (o_f * r)
+    if u_f != 1:
+        underest_fatal = (estimate_s <= 0) & (true_s > 0)
+        loss_s[underest_fatal] = (true_s[underest_fatal] - estimate_s) * (u_f * (r ** -0.5))
+    if o_f != 1:
+        overest_fatal = (estimate_s > 0) & (true_s <= 0)
+        loss_s[overest_fatal] = np.abs((true_s[overest_fatal] - estimate_s)) * (o_f * r)
     return loss_s
 
 def loss_sqr(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
@@ -78,42 +82,93 @@ def loss_sqr(estimate_s, true_s, u=1,o=1,u_f=1,o_f=1, r=1):
             Estimate_s has to be one single value.
 
     """
-    true_s = np.array(true_s).astype(float)
-    loss_s = np.zeros_like(true_s)
-    underest = (estimate_s < true_s)
-    underest_fatal = (estimate_s == 0) & (true_s > 0)
-    overest = (estimate_s > true_s)
-    overest_fatal = (estimate_s > 0) & (true_s == 0)
-    loss_s[underest] = (true_s[underest] - estimate_s)  * (u * r)
-    loss_s[underest_fatal] = (true_s[underest_fatal] - estimate_s) * (u_f * (r ** -0.5))
-    loss_s[overest] = (estimate_s - true_s[overest]) * (o * r)
-    loss_s[overest_fatal] = np.abs((true_s[overest_fatal] - estimate_s)) * (o_f * r)
-    return np.square(loss_s)
+    return np.square(loss_abs(estimate_s, true_s, u,o,u_f,o_f, r))
 
-def expected_loss(estimate_s, true_s, function='absolute', u=1,o=1,u_f=1,o_f=1, r=1):
+def expected_loss_for_estimate(estimate_s, true_s, function='absolute', u=1,o=1,u_f=1,o_f=1, r=1):
     """Function to attain expected loss, based on an absolute-error or
     squared error-loss function.
 
-    :param estimate_s:
-    :param true_s:
-    :param function:
-    :param u:
-    :param o:
-    :param u_f:
-    :param o_f:
-    :param r:
-    :return:
+        Args:
+            estimate_s (int or float): Score estimate.
+            true_s (int, float or np.array): True score value.
+            u (int or float, optional): Underestimation re-weighting factor.
+            o (int or float, optional): Overestimation re-weighting factor.
+            u_f (int or float, optional): Fatal underestimation re-weighting factor.
+            o_f (int or float, optional): Fatal overestimation re-weighting factor.
+            r (int, float or np.array, optional): Risk-affinity re-weighting factor.
+        Returns:
+            ...
+
     """
+
     if function == 'absolute':
         return loss_abs(estimate_s, true_s, u,o,u_f,o_f, r).mean()
     elif function == 'squared':
         return loss_sqr(estimate_s, true_s, u, o, u_f, o_f, r).mean()
     else:
         print('Error: Type of loss function not recognized. '
-              'Use "absolute" or "squared".)
+              'Use "absolute" or "squared".')
 
-def expected_loss_range(estimate_range, true_s, risk_range=np.array(1), function='absolute', u=1,o=1,u_f=1,o_f=1):
-    expected_loss_s = lambda estimate_s, r: expected_loss(estimate_s, true_s, function, u,o,u_f,o_f,r)
+def expected_loss_for_range(estimate_range, true_s, function='absolute', u=1,o=1,u_f=1,o_f=1, r=1):
+    """Function to attain expected loss and Bayes action based on an absolute-error or
+           squared error-loss function for a defined range of estimates.
 
-    for r in risk_range:
+                Args:
+                    estimate_range (np.array): Range of score estimates.
+                    true_s (np.array): Array of possible true values (from a probability distribution)
+                    u (int or float, optional): Underestimation re-weighting factor.
+                    o (int or float, optional): Overestimation re-weighting factor.
+                    u_f (int or float, optional): Fatal underestimation re-weighting factor.
+                    o_f (int or float, optional): Fatal overestimation re-weighting factor.
+                    r (int, float or np.array, optional): Risk-affinity re-weighting factor.
+                Returns:
+                    [0]: Expected loss for the range of estimates.
+                    [1]: Bayes action (estimate with minimal expected loss)
+                    [2]: Expected loss of Bayes action.
+        """
+    expected_loss_s = lambda estimate_s, r: expected_loss_for_estimate(estimate_s, true_s, function, u, o, u_f, o_f, r)
+    loss_e = [expected_loss_s(e, r) for e in estimate_range]
+    bayes_action = sop.fmin(expected_loss_s, -40, args=(r,), disp=False)
+    bayes_action_loss_e = expected_loss_s(bayes_action, r)
+    return loss_e, bayes_action, bayes_action_loss_e
 
+def expected_loss_plot(estimate_range, true_s, risk_range=1, function='absolute', u=1,o=1,u_f=1,o_f=1,
+                        verbose=False):
+    """Function to attain expected loss, based on an absolute-error or
+       squared error-loss function.
+
+            Args:
+                estimate_range (np.array): Range of score estimates.
+                true_s (np.array): Array of possible true values (from a probability distribution)
+                u (int or float, optional): Underestimation re-weighting factor.
+                o (int or float, optional): Overestimation re-weighting factor.
+                u_f (int or float, optional): Fatal underestimation re-weighting factor.
+                o_f (int or float, optional): Fatal overestimation re-weighting factor.
+                r (int, float or np.array, optional): Risk-affinity re-weighting factor.
+            Returns:
+                Plot of loss function realization for risk neutrality
+                or several risk factors.
+
+    """
+    ax = plt.subplot(111)
+    if isinstance(risk_range, (int,float)):
+        r_range=[risk_range]
+    else:
+        r_range=risk_range
+    for r in r_range:
+        loss_e, bayes_a, bayes_a_loss_e = expected_loss_for_range(estimate_range, true_s, function, u,o,u_f,o_f, r)
+        _color = next(ax._get_lines.prop_cycler)
+        plt.plot(estimate_range, loss_e, label="r =" + str(r), color=_color['color'])
+        plt.scatter(bayes_a, bayes_a_loss_e, s=70,
+                        color=_color['color'])  # , label = "Bayes action r "+str(r))
+        plt.vlines(bayes_a, 0, 10 * np.max(loss_e), color=_color['color'], linestyles="--")
+        if verbose == True:
+            print("Bayes action (minimum) at risk r %.2f: %.2f --- expected loss: %.2f"\
+                  % (r, bayes_a, bayes_a_loss_e))
+    plt.legend(loc="upper left", scatterpoints=1, title="Legend")
+    plt.xlabel("Score estimate")
+    plt.ylabel("Expected loss")
+    plt.xlim(estimate_range[0], estimate_range[-1])
+    plt.ylim(0, 1.1 * np.max(loss_e))
+    plt.grid()
+    plt.show()
