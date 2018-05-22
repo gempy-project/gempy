bade3c298d7bf0daf7b3c6a784d7b2ced5c029b7
fastamo <f.a.stamm@googlemail.com>
2018-05-22 14:05:33 +0200

    Gradient plotting function working Needs aspect ratio fixing

--- gempy/plotting/visualization.py
+++ gempy/plotting/visualization.py
@@ -377,44 +377,56 @@ class PlotData2D(object):
                          centroids[node][c2] * e2 / r2 + d2, str(node), color="white", size=6, ha="center", va="center",
                          weight="ultralight", family="monospace")
 
-    def plot_gradient(self, scalar_field, gx, gy, gz, cell_number, r=5,
-                      direction="y", plot_scalar = True, *args, **kwargs):
-        _a, _b, _c, extent_val, x, y, Gx, Gy = self._slice(direction, cell_number)
+    def plot_gradient(self, scalar_field, gx, gy, gz, cell_number, quiver_stepsize=5, #maybe call r sth. like "stepsize"?
+                      direction="y", plot_scalar = True, *args, **kwargs): #include plot data?
+        """
+            Plot the gradient of the scalar field in a given direction.
 
+            Args:
+                geo_data (gempy.DataManagement.InputData): Input data of the model
+                scalar_field(numpy.array): scalar field to plot with the gradient
+                gx(numpy.array): gradient in x-direction
+                gy(numpy.array): gradient in y-direction
+                gz(numpy.array): gradient in z-direction
+                cell_number(int): position of the array to plot
+                quiver_stepsize(int): step size between arrows to indicate gradient
+                direction(str): xyz. Caartesian direction to be plotted
+                plot_scalar(bool): boolean to plot scalar field
+                **kwargs: plt.contour kwargs
+
+            Returns:
+                None
+        """
         if direction == "y":
             if plot_scalar:
                 self.plot_scalar_field(scalar_field, cell_number, direction=direction, plot_data=False)
-            s2 = gx.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r,
-                 cell_number, ::r].T
-            V2 = gz.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r,
-                 cell_number, ::r].T
-            plt.quiver(self._data.grid.values[:, 0].reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r, cell_number, ::r].T,
-                   self._data.grid.values[:, 2].reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r, cell_number, ::r].T, s2, V2, pivot="tail",
+            U = gx.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::quiver_stepsize,
+                 cell_number, ::quiver_stepsize].T
+            V = gz.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::quiver_stepsize,
+                 cell_number, ::quiver_stepsize].T
+            plt.quiver(self._data.grid.values[:, 0].reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::quiver_stepsize, cell_number, ::quiver_stepsize].T,
+                   self._data.grid.values[:, 2].reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::quiver_stepsize, cell_number, ::quiver_stepsize].T, U, V, pivot="tail",
                    color='blue', alpha=.6)
         elif direction == "x":
             if plot_scalar:
                 self.plot_scalar_field(scalar_field, cell_number, direction=direction, plot_data=False)
-            s2 = gy.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r,
-                 cell_number, ::r].T
-            V2 = gz.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r,
-                 cell_number, ::r].T
-            plt.quiver(self._data.grid.values[:, 0].reshape(self._data.resolution[0], self._data.resolution[1],
-                                                            self._data.resolution[2])[::r, cell_number, ::r].T,
+            U = gy.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[cell_number, ::quiver_stepsize, ::quiver_stepsize].T
+            V = gz.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[cell_number, ::quiver_stepsize, ::quiver_stepsize].T
+            plt.quiver(self._data.grid.values[:, 1].reshape(self._data.resolution[0], self._data.resolution[1],
+                                                            self._data.resolution[2])[cell_number, ::quiver_stepsize,  ::quiver_stepsize].T,
                        self._data.grid.values[:, 2].reshape(self._data.resolution[0], self._data.resolution[1],
-                                                            self._data.resolution[2])[::r, cell_number, ::r].T, s2, V2,
+                                                            self._data.resolution[2])[cell_number, ::quiver_stepsize,  ::quiver_stepsize].T, U, V,
                        pivot="tail",
                        color='blue', alpha=.6)
         elif direction== "z":
             if plot_scalar:
                 self.plot_scalar_field(scalar_field, cell_number, direction=direction, plot_data=False)
-            s2 = gx.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r,
-                 cell_number, ::r].T
-            V2 = gy.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::r,
-                 cell_number, ::r].T
+            U = gx.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::quiver_stepsize, ::quiver_stepsize, cell_number].T
+            V = gy.reshape(self._data.resolution[0], self._data.resolution[1], self._data.resolution[2])[::quiver_stepsize, ::quiver_stepsize, cell_number].T
             plt.quiver(self._data.grid.values[:, 0].reshape(self._data.resolution[0], self._data.resolution[1],
-                                                            self._data.resolution[2])[::r, cell_number, ::r].T,
-                       self._data.grid.values[:, 2].reshape(self._data.resolution[0], self._data.resolution[1],
-                                                            self._data.resolution[2])[::r, cell_number, ::r].T, s2, V2,
+                                                            self._data.resolution[2])[::quiver_stepsize, ::quiver_stepsize, cell_number].T,
+                       self._data.grid.values[:, 1].reshape(self._data.resolution[0], self._data.resolution[1],
+                                                            self._data.resolution[2])[::quiver_stepsize, ::quiver_stepsize, cell_number].T, U, V,
                        pivot="tail",
                        color='blue', alpha=.6)
         else:
