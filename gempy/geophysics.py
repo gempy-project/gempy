3b42f4b84d82097d7b58ed85294d35853be53587
saratlh <33130621+saratlh@users.noreply.github.com>
2018-05-23 12:43:31 +0200

    add t matrix calculation function

--- gempy/geophysics.py
+++ gempy/geophysics.py
@@ -161,6 +161,95 @@ class Preprocessing(object):
 
         return vx_all, vy_all, vz_all, np.ravel(self.b_all)
 
+    def get_T_mat(self, Xn, Yn, Zn, rxLoc, n_chunck_o):
+        eps = 1e-10  # add a small value to the locations to avoid /0
+
+        nC = Xn.shape[1]
+
+        # Pre-allocate space for 1D array
+        Tx = np.zeros((n_chunck_o, 3 * nC))
+        Ty = np.zeros((n_chunck_o, 3 * nC))
+        Tz = np.zeros((n_chunck_o, 3 * nC))
+
+        dz2 = np.repeat(rxLoc[:,2], nC).reshape(n_chunck_o,-1) - Zn[:, :, 0]
+        dz1 = np.repeat(rxLoc[:,2], nC).reshape(n_chunck_o,-1) - Zn[:, :, 1]
+
+        dy2 = Yn[:, :, 1] - np.repeat(rxLoc[:, 1], nC).reshape(n_chunck_o, -1)
+        dy1 = Yn[:, :, 0] - np.repeat(rxLoc[:, 1], nC).reshape(n_chunck_o, -1)
+
+        dx2 = Xn[:, :, 1] - np.repeat(rxLoc[:, 0], nC).reshape(n_chunck_o, -1)
+        dx1 = Xn[:, :, 0] - np.repeat(rxLoc[:, 0], nC).reshape(n_chunck_o, -1)
+
+        R1 = (dy2 ** 2 + dx2 ** 2) + eps
+        R2 = (dy2 ** 2 + dx1 ** 2) + eps
+        R3 = (dy1 ** 2 + dx2 ** 2) + eps
+        R4 = (dy1 ** 2 + dx1 ** 2) + eps
+
+        arg1 = np.sqrt(dz2 ** 2 + R2)
+        arg2 = np.sqrt(dz2 ** 2 + R1)
+        arg3 = np.sqrt(dz1 ** 2 + R1)
+        arg4 = np.sqrt(dz1 ** 2 + R2)
+        arg5 = np.sqrt(dz2 ** 2 + R3)
+        arg6 = np.sqrt(dz2 ** 2 + R4)
+        arg7 = np.sqrt(dz1 ** 2 + R4)
+        arg8 = np.sqrt(dz1 ** 2 + R3)
+
+        Tx[:, 0:nC] = np.arctan2(dy1 * dz2, (dx2 * arg5)) + \
+                      - np.arctan2(dy2 * dz2, (dx2 * arg2)) + \
+                      np.arctan2(dy2 * dz1, (dx2 * arg3)) + \
+                      - np.arctan2(dy1 * dz1, (dx2 * arg8)) + \
+                      np.arctan2(dy2 * dz2, (dx1 * arg1)) + \
+                      - np.arctan2(dy1 * dz2, (dx1 * arg6)) + \
+                      np.arctan2(dy1 * dz1, (dx1 * arg7)) + \
+                      - np.arctan2(dy2 * dz1, (dx1 * arg4))
+
+        Ty[0, 0:nC] = np.log((dz2 + arg2) / (dz1 + arg3)) + \
+                      -np.log((dz2 + arg1) / (dz1 + arg4)) + \
+                      np.log((dz2 + arg6) / (dz1 + arg7)) + \
+                      -np.log((dz2 + arg5) / (dz1 + arg8))
+
+        Ty[0, nC:2 * nC] = np.arctan2(dx1 * dz2, (dy2 * arg1)) + \
+                           - np.arctan2(dx2 * dz2, (dy2 * arg2)) + \
+                           np.arctan2(dx2 * dz1, (dy2 * arg3)) + \
+                           - np.arctan2(dx1 * dz1, (dy2 * arg4)) + \
+                           np.arctan2(dx2 * dz2, (dy1 * arg5)) + \
+                           - np.arctan2(dx1 * dz2, (dy1 * arg6)) + \
+                           np.arctan2(dx1 * dz1, (dy1 * arg7)) + \
+                           - np.arctan2(dx2 * dz1, (dy1 * arg8))
+
+        R1 = (dy2 ** 2 + dz1 ** 2) + eps
+        R2 = (dy2 ** 2 + dz2 ** 2) + eps
+        R3 = (dy1 ** 2 + dz1 ** 2) + eps
+        R4 = (dy1 ** 2 + dz2 ** 2) + eps
+
+        Ty[0, 2 * nC:] = np.log((dx1 + np.sqrt(dx1 ** 2 + R1)) /
+                                (dx2 + np.sqrt(dx2 ** 2 + R1))) + \
+                         -np.log((dx1 + np.sqrt(dx1 ** 2 + R2)) / (dx2 + np.sqrt(dx2 ** 2 + R2))) + \
+                         np.log((dx1 + np.sqrt(dx1 ** 2 + R4)) / (dx2 + np.sqrt(dx2 ** 2 + R4))) + \
+                         -np.log((dx1 + np.sqrt(dx1 ** 2 + R3)) / (dx2 + np.sqrt(dx2 ** 2 + R3)))
+
+        R1 = (dx2 ** 2 + dz1 ** 2) + eps
+        R2 = (dx2 ** 2 + dz2 ** 2) + eps
+        R3 = (dx1 ** 2 + dz1 ** 2) + eps
+        R4 = (dx1 ** 2 + dz2 ** 2) + eps
+
+        Tx[0, 2 * nC:] = np.log((dy1 + np.sqrt(dy1 ** 2 + R1)) /
+                                (dy2 + np.sqrt(dy2 ** 2 + R1))) + \
+                         -np.log((dy1 + np.sqrt(dy1 ** 2 + R2)) / (dy2 + np.sqrt(dy2 ** 2 + R2))) + \
+                         np.log((dy1 + np.sqrt(dy1 ** 2 + R4)) / (dy2 + np.sqrt(dy2 ** 2 + R4))) + \
+                         -np.log((dy1 + np.sqrt(dy1 ** 2 + R3)) / (dy2 + np.sqrt(dy2 ** 2 + R3)))
+
+        Tz[0, 2 * nC:] = -(Ty[0, nC:2 * nC] + Tx[0, 0:nC])
+        Tz[0, nC:2 * nC] = Ty[0, 2 * nC:]
+        Tx[0, nC:2 * nC] = Ty[0, 0:nC]
+        Tz[0, 0:nC] = Tx[0, 2 * nC:]
+
+        Tx = Tx / (4 * np.pi)
+        Ty = Ty / (4 * np.pi)
+        Tz = Tz / (4 * np.pi)
+
+        return Tx, Ty, Tz
+
     def default_range(self):
         # Max range to select voxels
         range_ = (self.model_grid[:, 2].max() - self.model_grid[:, 2].min()) * 0.9
