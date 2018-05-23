b5db26de287910e69b972335ef4aaf45fd6ef4a8
saratlh <33130621+saratlh@users.noreply.github.com>
2018-05-23 12:00:38 +0200

    change geophysics.py's class name to include magnetic calculation

--- gempy/interpolator.py
+++ gempy/interpolator.py
@@ -146,7 +146,7 @@ class InterpolatorData:
                                     on_unused_input='ignore',
                                     allow_input_downcast=False,
                                     profile=False)
-            
+
         elif output is 'magnetic':
             # then we compile we have to pass the number of formations that are faults!!
             th_fn = theano.function(input_data_T,
@@ -307,8 +307,8 @@ class InterpolatorData:
     # =======
     # Gravity
     def create_geophysics_obj(self, ai_extent, ai_resolution, ai_z=None, range_max=None):
-        from .geophysics import GravityPreprocessing
-        self.geophy = GravityPreprocessing(self, ai_extent, ai_resolution, ai_z=ai_z, range_max=range_max)
+        from .geophysics import Preprocessing
+        self.geophy = Preprocessing(self, ai_extent, ai_resolution, ai_z=ai_z, range_max=range_max)
 
     def set_gravity_precomputation(self, gravity_obj):
         """
