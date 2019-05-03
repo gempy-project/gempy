ef4710fdfd18e7b8affdaca01b01aea944128b72
Miguel <varga@aices.rwth-aachen.de>
2019-05-03 16:13:42 +0200

    [DOC]
    - try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@ import os
 import sys
 import IPython.sphinxext
 from pygments.plugin import find_plugin_lexers
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../gempy/'))
 sys.path.insert(0, os.path.abspath('../../gempy/core/'))
 
