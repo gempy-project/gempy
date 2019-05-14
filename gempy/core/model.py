53202cedf876a2acd9ad136c0c20960a530f1f93
varga <varga@aices.rwth-aachen.de>
2019-05-14 09:04:42 +0200

    [BUG]
    - Fixed sequential pile deprecation

--- gempy/core/model.py
+++ gempy/core/model.py
@@ -335,7 +335,7 @@ class DataMutation(object):
             aux = self.series.df.index.drop('Basement').get_values()
             self.reorder_series(np.append(aux, 'Basement'))
 
-        return self.surfaces.sequential_pile.figure
+        return self.surfaces
 
     # endregion
 
