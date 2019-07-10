b9e3bb350ef7f3dac4ecdfcb06a8a59c9f40da04
elimh <elisa.heim@gmx.de>
2019-07-10 14:43:55 +0200

    added and modified some plotting options

--- gempy/core/grid_modules/grid_types.py
+++ gempy/core/grid_modules/grid_types.py
@@ -456,29 +456,26 @@ class Topography:
 
     def _line_in_section(self, direction='y', cell_number=1):
         # todo delete after replacing it with the other function
-        if type(direction) == list:
-            pass
 
-        else:
-            x = self.values_3D_res[:, :, 0]
-            y = self.values_3D_res[:, :, 1]
-            z = self.values_3D_res[:, :, 2]
+        x = self.values_3D_res[:, :, 0]
+        y = self.values_3D_res[:, :, 1]
+        z = self.values_3D_res[:, :, 2]
 
-            if direction == 'y':
-                a = x[cell_number, :]
-                b = y[cell_number, :]
-                c = z[cell_number, :]
-                assert len(np.unique(b)) == 1
-                topoline = np.dstack((a, c)).reshape(-1, 2).astype(int)
+        if direction == 'y':
+            a = x[cell_number, :]
+            b = y[cell_number, :]
+            c = z[cell_number, :]
+            assert len(np.unique(b)) == 1
+            topoline = np.dstack((a, c)).reshape(-1, 2).astype(int)
 
-            elif direction == 'x':
-                a = x[:, cell_number]
-                b = y[:, cell_number]
-                c = z[:, cell_number]
-                assert len(np.unique(a)) == 1
-                topoline = np.dstack((b, c)).reshape(-1, 2).astype(int)
+        elif direction == 'x':
+            a = x[:, cell_number]
+            b = y[:, cell_number]
+            c = z[:, cell_number]
+            assert len(np.unique(a)) == 1
+            topoline = np.dstack((b, c)).reshape(-1, 2).astype(int)
 
-            elif direction == "z":
-                raise NotImplementedError
+        elif direction == "z":
+            raise NotImplementedError
 
         return topoline
