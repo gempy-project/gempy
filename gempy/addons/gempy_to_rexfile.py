9968102c65a4e683d54fed9eb63170de842c9541
varga <varga@aices.rwth-aachen.de>
2020-02-22 15:58:31 +0100

    [ENH] First running prototype gempy_to_rexfile.py

--- gempy/addons/gempy_to_rexfile.py
+++ gempy/addons/gempy_to_rexfile.py
@@ -23,7 +23,14 @@
 import numpy as np
 
 rexFileHeaderSize = 64
+rexCoordSize = 22
+
+file_header_size = 86
 rexDataBlockHeaderSize = 16
+
+file_header_and_data_header = 102
+mesh_header_size = 128
+all_header_size = 230
 
 # Supported block types
 # typeLineSet = 0
@@ -37,11 +44,17 @@ typeMesh = 3
 # typeSceneNode = 8
 
 
+n_bytes = 0
+
+
 def encode(input_: list):
+    global n_bytes
     block = bytearray()
 
     for tup in input_:
-        block += np.array(tup[0], dtype=tup[1]).tobytes()
+        arr = np.array(tup[0], dtype=tup[1]).tobytes()
+        n_bytes += len(arr)
+        block += arr
 
     return block
 
@@ -60,7 +73,7 @@ def write_header_block(n_data_blocks, size_data_blocks, version=1, start_data=86
     Returns:
 
     """
-    reserved = [0]*42
+    reserved = '0'*42
     if offsets is None:
         offsets = [0, 0, 0]
 
@@ -72,6 +85,7 @@ def write_header_block(n_data_blocks, size_data_blocks, version=1, start_data=86
               (size_data_blocks, 'uint64'),
               (reserved, 'bytes'),
               (3876, 'uint32'),
+              (4, 'uint16'),
               ('EPSG', 'bytes'),
               (offsets, 'float32')]
 
@@ -104,7 +118,7 @@ def write_data_block(size_data, data_id = 1, data_type=3, version_data=1):
 
 def write_mesh_header(n_vtx_coord,  n_triangles,
                       start_vtx_coord, start_nor_coord, start_tex_coord, start_vtx_colors, start_triangles,
-                      name, material_id = 0,
+                      name, material_id=9223372036854775807,
                       n_nor_coord=0, n_tex_coord=0, n_vtx_colors=0,
                       lod=1, max_lod=1):
     """
@@ -130,12 +144,12 @@ def write_mesh_header(n_vtx_coord,  n_triangles,
     """
 
     str_size = len(name)
-    full_name = ' ' * 74
-    full_name[:str_size] = name
+    rest_name_ = ' ' * (74 - str_size)
+    full_name = name + rest_name_
 
     input_ = [([lod, max_lod], 'uint16'),
-              ([n_vtx_coord, n_nor_coord, n_tex_coord, n_vtx_colors, n_triangles,
-                start_vtx_coord, start_tex_coord, start_nor_coord, start_tex_coord, start_vtx_colors, start_triangles],
+              ([n_vtx_coord,     n_nor_coord,     n_tex_coord,     n_vtx_colors,     n_triangles,
+                start_vtx_coord, start_nor_coord, start_tex_coord, start_vtx_colors, start_triangles],
                'uint32'),
               (material_id, 'uint64'),
               (str_size, 'uint16'),
@@ -179,12 +193,64 @@ def write_mesh_coordinates(vertex, triangles, normal=None, texture=None, colors=
     return block_bytes
 
 
-def write_file(bytes, name: str):
-    newFile = open(name+".rex", "wb")
+def mesh_preprocess(ver, tri):
+    ver_ravel = ver.ravel()
+    tri_ravel = tri.ravel()
+    n_vtx_coord = ver_ravel.shape[0]
+    n_triangles = tri_ravel.shape[0]
+    return ver_ravel, tri_ravel, n_vtx_coord, n_triangles
+
+
+def write_file(bytes, path: str):
+    newFile = open(path + ".rex", "wb")
     newFile.write(bytes)
     return True
 
 
+def geo_model_to_res(geo_model, path='./gempy_rex'):
+
+    mesh_header_size = 128
+    file_header_size = 86
+    e = 0
+    for ver, tri in zip(geo_model.solutions.vertices, geo_model.solutions.edges):
+
+        colors = np.ones_like(ver) * 200
+
+        ver_ = np.copy(ver)
+        ver_[:, 2] = ver[:, 1]
+        ver_[:, 1] = ver[:, 2]
+
+        tri_ = np.copy(tri)
+        tri_[:, 2] = tri[:, 1]
+        tri_[:, 1] = tri[:, 2]
+
+        ver_ravel, tri_ravel, n_vtx_coord, n_triangles = mesh_preprocess(ver_, tri_)
+        data_block_size_no_header = 2 * (n_vtx_coord + n_triangles) * 4 + mesh_header_size
+
+        # Write header
+        header_bytes = write_header_block(n_data_blocks=1,
+                                          size_data_blocks=data_block_size_no_header + rexDataBlockHeaderSize,
+                                          start_data=file_header_size)
+
+        # Write data block
+        data_bytes = write_data_block(size_data=data_block_size_no_header,
+                                      data_id=1, data_type=3, version_data=1)
+
+        # Write mesh block
+        mesh_header_bytes = write_mesh_header(n_vtx_coord / 3, n_triangles / 3, n_vtx_colors=n_vtx_coord/3,
+                                              start_vtx_coord=mesh_header_size,
+                                              start_nor_coord=mesh_header_size + n_vtx_coord * 4,
+                                              start_tex_coord=mesh_header_size + n_vtx_coord * 4,
+                                              start_vtx_colors=mesh_header_size + n_vtx_coord * 4,
+                                              start_triangles=mesh_header_size + 2 * (n_vtx_coord * 4),
+                                              name='test_a')
+
+        mesh_block_bytes = write_mesh_coordinates(ver_ravel, tri_ravel, colors=colors.ravel())
+
+        all_bytes = header_bytes + data_bytes + mesh_header_bytes + mesh_block_bytes
+
+        write_file(all_bytes, path+str(e))
+        e += 1
 
 
 
---
+++ test/test_addons/test_gempy_to_rexfile.py
@@ -0,0 +1,70 @@
+import pytest
+
+import numpy as np
+import sys, os
+sys.path.append("../..")
+import gempy
+import matplotlib.pyplot as plt
+from gempy.addons import gempy_to_rexfile as gtr
+
+input_path = os.path.dirname(__file__)+'/../input_data'
+
+
+class TestGemPyToREX:
+    @pytest.fixture(scope='module')
+    def geo_model(self, interpolator_islith_nofault):
+        """
+        2 Horizontal layers with drift 0
+        """
+        # Importing the data from csv files and settign extent and resolution
+        geo_data = gempy.create_data([0, 10, 0, 10, -10, 0], [50, 50, 50],
+                                     path_o=input_path + "/GeoModeller/test_a/test_a_Foliations.csv",
+                                     path_i=input_path + "/GeoModeller/test_a/test_a_Points.csv")
+
+        geo_data.set_theano_function(interpolator_islith_nofault)
+
+        # Compute model
+        sol = gempy.compute_model(geo_data)
+
+        return geo_data
+
+    def test_write_header(self):
+
+        header_bytes = gtr.write_header_block(3, 1)
+        gtr.write_file(header_bytes, './rexfiles/header_test')
+
+    def test_write_mesh(self, geo_model):
+        mesh_header_size = 128
+        file_header_size = 86
+        ver, tri = geo_model.solutions.vertices[0], geo_model.solutions.edges[0]
+
+        ver_ravel, tri_ravel, n_vtx_coord, n_triangles = gtr.mesh_preprocess(ver, tri)
+        data_block_size_no_header = (n_vtx_coord + n_triangles) * 4 + mesh_header_size
+
+        # Write header
+        header_bytes = gtr.write_header_block(n_data_blocks=1,
+                                              size_data_blocks=data_block_size_no_header+gtr.rexDataBlockHeaderSize,
+                                              start_data=file_header_size)
+
+        # Write data block
+        data_bytes = gtr.write_data_block(size_data=data_block_size_no_header,
+                                          data_id=1, data_type=3, version_data=1)
+
+        # Write mesh block
+        mesh_header_bytes = gtr.write_mesh_header(n_vtx_coord/3, n_triangles/3,
+                                                  start_vtx_coord=gtr.mesh_header_size,
+                                                  start_nor_coord=gtr.mesh_header_size + n_vtx_coord*4,
+                                                  start_tex_coord=gtr.mesh_header_size + n_vtx_coord*4,
+                                                  start_vtx_colors=gtr.mesh_header_size + n_vtx_coord*4,
+                                                  start_triangles=gtr.mesh_header_size + n_vtx_coord*4,
+                                                  name='test_a')
+
+        mesh_block_bytes = gtr.write_mesh_coordinates(ver_ravel, tri_ravel)
+
+        all_bytes = header_bytes + data_bytes + mesh_header_bytes + mesh_block_bytes
+
+        gtr.write_file(all_bytes, './rexfiles/one_mesh_test')
+
+    def test_geo_model_to_rex(self, geo_model):
+
+        gtr.geo_model_to_res(geo_model, path='./rexfiles/gtr_test')
