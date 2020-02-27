20f416d3c6cbf723b136755c86a9bef3dbf617a9
Florian Wellmann <flohorovicic@gmail.com>
2020-02-27 14:08:05 +0100

    Added module for geographic data handling

---
+++ gempy/assets/geographic.py
@@ -0,0 +1,10 @@
+""" Method to handle geographic information
+
+General set of methods to handle geographic inforamtion and
+additional data sets (e.g. GoogleEarth .kml files, GeoTiffs, etc.)
+
+2020 Florian Wellmann
+"""
+
+
+
