cdbdeb24be0b6d2c0c2f023df2edb8fee7604dc2
pytzcarraldo <a.schaaf@abdn.ac.uk>
2020-02-27 16:10:16 +0100

    [PEP8] compliance

--- gempy/plot/plot_api.py
+++ gempy/plot/plot_api.py
@@ -23,36 +23,34 @@
     @author: Alex Schaaf, Elisa Heim, Miguel de la Varga
 """
 
-from os import path
-import sys
-
 # This is for sphenix to find the packages
-#sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+# sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
-from .visualization_2d_pro import Plot2D
+from typing import Union
+
+import matplotlib.pyplot as plt
 # from .vista import Vista
 # import gempy as _gempy
 import numpy as np
 import pandas as pn
-import matplotlib.pyplot as plt
-from typing import Union
-from .vista import Vista
-import gempy as _gempy
-from typing import Set, Tuple, Dict, Union
-from nptyping import Array
 
+from .vista import Vista
+from .visualization_2d_pro import Plot2D
 
 try:
     import mplstereonet
+
     mplstereonet_import = True
 except ImportError:
     mplstereonet_import = False
 
 
-def plot_2d(model, n_axis=None, section_names:list = None, cell_number: list = None, direction: list = 'y',
-            show_data: Union[bool, list] = True, show_lith: Union[bool, list] = True,
-            show_scalar: Union[bool, list] = False, show_boundaries: Union[bool, list] = True, **kwargs):
-
+def plot_2d(model, n_axis=None, section_names: list = None,
+            cell_number: list = None, direction: list = 'y',
+            show_data: Union[bool, list] = True,
+            show_lith: Union[bool, list] = True,
+            show_scalar: Union[bool, list] = False,
+            show_boundaries: Union[bool, list] = True, **kwargs):
     section_names = [] if section_names is None else section_names
     section_names = np.atleast_1d(section_names)
     if cell_number is None:
@@ -88,8 +86,8 @@ def plot_2d(model, n_axis=None, section_names:list = None, cell_number: list = N
     for e, sn in enumerate(section_names):
         assert e < 10, 'Reached maximum of axes'
 
-        ax_pos = (int(n_axis/2)+1)*100+20+e+1
-        #print(ax_pos, '1')
+        ax_pos = (int(n_axis / 2) + 1) * 100 + 20 + e + 1
+        # print(ax_pos, '1')
         temp_ax = p.add_section(section_name=sn, ax_pos=ax_pos, **kwargs)
         if show_data[e] is True:
             p.plot_data(temp_ax, section_name=sn, **kwargs)
@@ -104,34 +102,42 @@ def plot_2d(model, n_axis=None, section_names:list = None, cell_number: list = N
         e = e + 1
 
     for e2 in range(len(cell_number)):
-        assert (e+e2) < 10, 'Reached maximum of axes'
+        assert (e + e2) < 10, 'Reached maximum of axes'
 
-        ax_pos = (int(n_axis/2)+1)*100+20+e+e2+1
+        ax_pos = (int(n_axis / 2) + 1) * 100 + 20 + e + e2 + 1
         print(ax_pos)
 
-        temp_ax = p.add_section(cell_number=cell_number[e2], direction=direction[e2], ax_pos=ax_pos)
-        if show_data[e+e2] is True:
-            p.plot_data(temp_ax, cell_number=cell_number[e2], direction=direction[e2], **kwargs)
-        if show_lith[e+e2] is True:
-            p.plot_lith(temp_ax, cell_number=cell_number[e2], direction=direction[e2], **kwargs)
-        if show_scalar[e+e2] is True:
-            p.plot_scalar_field(temp_ax, cell_number=cell_number[e2], direction=direction[e2], **kwargs)
-        if show_boundaries[e+e2] is True:
-            p.plot_contacts(temp_ax, cell_number=cell_number[e2], direction=direction[e2], **kwargs)
+        temp_ax = p.add_section(cell_number=cell_number[e2],
+                                direction=direction[e2], ax_pos=ax_pos)
+        if show_data[e + e2] is True:
+            p.plot_data(temp_ax, cell_number=cell_number[e2],
+                        direction=direction[e2], **kwargs)
+        if show_lith[e + e2] is True:
+            p.plot_lith(temp_ax, cell_number=cell_number[e2],
+                        direction=direction[e2], **kwargs)
+        if show_scalar[e + e2] is True:
+            p.plot_scalar_field(temp_ax, cell_number=cell_number[e2],
+                                direction=direction[e2], **kwargs)
+        if show_boundaries[e + e2] is True:
+            p.plot_contacts(temp_ax, cell_number=cell_number[e2],
+                            direction=direction[e2], **kwargs)
 
     return p
 
 
 def plot_section_traces(model):
-    pst = plot_2d(model, n_axis=1, section_names=['topography'], show_data=False, show_boundaries=False)
+    pst = plot_2d(model, n_axis=1, section_names=['topography'],
+                  show_data=False, show_boundaries=False)
     pst.plot_section_traces(pst.axes[0], show_data=False)
     return pst
 
 
-def plot_stereonet(self, litho=None, planes=True, poles=True, single_plots=False,
+def plot_stereonet(self, litho=None, planes=True, poles=True,
+                   single_plots=False,
                    show_density=False):
     if mplstereonet_import is False:
-        raise ImportError('mplstereonet package is not installed. No stereographic projection available.')
+        raise ImportError(
+            'mplstereonet package is not installed. No stereographic projection available.')
 
     from collections import OrderedDict
 
@@ -142,7 +148,9 @@ def plot_stereonet(self, litho=None, planes=True, poles=True, single_plots=False
         fig, ax = mplstereonet.subplots(figsize=(5, 5))
         df_sub2 = pn.DataFrame()
         for i in litho:
-            df_sub2 = df_sub2.append(self.model.orientations.df[self.model.orientations.df['surface'] == i])
+            df_sub2 = df_sub2.append(self.model.orientations.df[
+                                         self.model.orientations.df[
+                                             'surface'] == i])
 
     for formation in litho:
         if single_plots:
@@ -153,21 +161,27 @@ def plot_stereonet(self, litho=None, planes=True, poles=True, single_plots=False
         # if series_only:
         # df_sub = self.model.orientations.df[self.model.orientations.df['series'] == formation]
         # else:
-        df_sub = self.model.orientations.df[self.model.orientations.df['surface'] == formation]
+        df_sub = self.model.orientations.df[
+            self.model.orientations.df['surface'] == formation]
 
         if poles:
-            ax.pole(df_sub['azimuth'] - 90, df_sub['dip'], marker='o', markersize=7,
+            ax.pole(df_sub['azimuth'] - 90, df_sub['dip'], marker='o',
+                    markersize=7,
                     markerfacecolor=self._color_lot[formation],
-                    markeredgewidth=1.1, markeredgecolor='gray', label=formation + ': ' + 'pole point')
+                    markeredgewidth=1.1, markeredgecolor='gray',
+                    label=formation + ': ' + 'pole point')
         if planes:
-            ax.plane(df_sub['azimuth'] - 90, df_sub['dip'], color=self._color_lot[formation],
+            ax.plane(df_sub['azimuth'] - 90, df_sub['dip'],
+                     color=self._color_lot[formation],
                      linewidth=1.5, label=formation + ': ' + 'azimuth/dip')
         if show_density:
             if single_plots:
                 ax.density_contourf(df_sub['azimuth'] - 90, df_sub['dip'],
-                                    measurement='poles', cmap='viridis', alpha=.5)
+                                    measurement='poles', cmap='viridis',
+                                    alpha=.5)
             else:
-                ax.density_contourf(df_sub2['azimuth'] - 90, df_sub2['dip'], measurement='poles', cmap='viridis',
+                ax.density_contourf(df_sub2['azimuth'] - 90, df_sub2['dip'],
+                                    measurement='poles', cmap='viridis',
                                     alpha=.5)
 
         fig.subplots_adjust(top=0.8)
