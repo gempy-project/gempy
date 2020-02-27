b3c1234e47bc111f935411f00ca59c3c702063e8
GerogeLiang3 <liangzhouji@gmail.com>
2020-02-27 14:45:45 +0100

    pep 8 format

--- test/test_core/test_tensorflow.py
+++ test/test_core/test_tensorflow.py
@@ -4,9 +4,13 @@ import tensorflow as tf
 
 
 def test_basic_tf():
-    dips_position = tf.constant([[1,0],[1,1]],dtype = tf.float32)
-    surface_position = tf.constant([[[0,0],[2,0]],[[0,2],[2,2]]],dtype = tf.float32)
-    dips = tf.constant([[1,1],[1,1]],dtype= tf.float32)
+    dips_position = tf.constant([[1,0],
+                                 [1,1]],
+                                dtype = tf.float32)
+    surface_position = tf.constant([[[0,0],[2,0]],
+                                    [[0,2],[2,2]]],dtype = tf.float32)
+    dips = tf.constant([[1,1],
+                        [1,1]],dtype= tf.float32)
 
     test_model = Tensorflow_kriging_2D(dips_position,surface_position,dips)
 
