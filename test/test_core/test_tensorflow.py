d6d6b8a392769e6866cc5d62b8bd8f358ef1dcb0
GerogeLiang3 <liangzhouji@gmail.com>
2020-02-27 13:07:54 +0100

    add first tensorflow function squared_euclidean_distance

--- test/test_core/test_tensorflow.py
+++ test/test_core/test_tensorflow.py
@@ -1,5 +1,16 @@
-from gempy.core.interp_methods.tf_2D import constant
+from gempy.core.interp_methods.tf_2D import Tensorflow_kriging_2D
+import tensorflow as tf
 
-def test_tf(val = 2):
-    assert constant(val) == 2
+
+
+def test_basic_tf():
+    dips_position = tf.constant([[1,0],[1,1]])
+    surface_position = tf.constant([[[0,0],[2,0]],[[0,2],[2,2]]])
+    dips = tf.constant([[1,1],[1,1]])
+
+    test_model = Tensorflow_kriging_2D(dips_position,surface_position,dips)
+
+    # test_model.squared_euclidean_distance(surface_position,surface_position)
+
+
 
