d78d95a01097d5405f1937fae22444861f3113b6
Simon Virgo <simon.virgo@rwth-aachen.de>
2020-03-02 14:21:50 +0100

    added method to create and display rextag

--- gempy/addons/rex_api.py
+++ gempy/addons/rex_api.py
@@ -2,6 +2,8 @@ import os
 import requests
 import json
 import datetime
+import pyqrcode as qr
+
 package_directory = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -13,13 +15,17 @@ class RexAPI:
         self.secret = None
         self.owner = None
         self.access_token = None
+        self.project_name = None
         self.project_urn = None
         self.project_link = None
-        self.root_reference = None
+        self.project_reference = None
         self.root_reference_link = None
+        self.root_reference_key = None
         self.file_reference_link = None
         self.project_file_urn = None
         self.project_file_uplink = None
+        self.rextag_url = None
+        self.rextag = None
 
         self.response = None  # saves the most current server response for debugging
 
@@ -88,6 +94,7 @@ class RexAPI:
         self.response = requests.post('https://rex.robotic-eyes.com/api/v2/rexReferences', headers=headers, data=data)
         if self.response.status_code == 201:
             self.root_reference_link = self.response.json()['_links']['self']['href']
+            self.root_reference_key = self.response.json()['key']
 
         else:
             print("something went wrong! Status code: " + str(self.response.status_code))
@@ -111,7 +118,8 @@ class RexAPI:
             print("something went wrong! Status code: " + str(self.response.status_code))
 
 
-    def create_project_file(self, filename):
+    def create_project_file(self, projectname):
+        self.project_name = projectname
         headers = {
             'Authorization':'Bearer ' + self.access_token,
             'Accept': 'application/json;charset=UTF-8',
@@ -119,7 +127,7 @@ class RexAPI:
                  }
 
         data = json.dumps({"project" : self.project_link,
-                           "name" : filename,
+                           "name" : self.project_name,
                            "type" : "rex",
                            "rexReference" : self.file_reference_link
                            })
@@ -129,6 +137,7 @@ class RexAPI:
         if self.response.status_code == 201:
             self.project_file_urn = self.response.json()['urn']
             self.project_file_uplink = self.response.json()['_links']['file.upload']['href']
+            self.project_reference = self.response.json()['rexReferenceKey']
 
         else:
             print("something went wrong! Status code: " + str(self.response.status_code))
@@ -158,6 +167,33 @@ class RexAPI:
 
         self.response = requests.get(self.project_link + '/projectFiles', headers=headers)
 
+    def show_tag(self, reverse=True):
+        """
+        displays the rextag to the terminal standard output as ascii.
+
+        you can invert the color by setting inverse=True, this is necessary if you run it in a jupyter notebook
+        after creation, you can save the rextag as svg using the rextag.svg method:
+        self.rextag.svg("project_name", scale=8)
+        
+        Args:
+            reverse: (boolean) inverts background and foreground color in the terminal output
+
+        Returns: None, prints a QR code to terminal output
+
+        """
+
+        base_url = "https://rex.codes/v1/"
+        self.rextag_url = base_url+self.project_reference
+        self.rextag = qr.create(self.rextag_url)
+        #self.rextag.svg(self.project_name, scale=8)
+        #self.rextag.eps(self.project_name, scale=2)
+
+        if reverse:
+            print(self.rextag.terminal(module_color="reverse", background="default", quiet_zone=1))
+
+        else:
+            print(self.rextag.terminal(quiet_zone=1))
+
 
     def upload_rexfiles(self, infiles : list):
         """
@@ -169,6 +205,8 @@ class RexAPI:
         the function will take a list of rexos input filenames and uploads them into a newly created project.
 
         an ar code is plotted that can be scanned with the rexview app to show the model in vr.
+        the qr code is plotted in ascii to the standard output, if the qr code is not crecognizable,
+         try to revert the colors o0f the terminal output by setting reverse=False in thew show_tag() Method.
 
         all api calls are python implementation of the Rex os api:
         https://www.rexos.org/rex-api/#tutorial-rex-project-before-you-begin
