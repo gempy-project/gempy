41ff0e3f4e7a4f00be9a08fc0ac9c7d7c1b3d811
Florian Wellmann <flohorovicic@gmail.com>
2020-04-11 21:31:41 +0200

    Added tests for grid generation and point selection

--- test/test_utilities/test_diamond_square.py
+++ test/test_utilities/test_diamond_square.py
@@ -1,23 +1,68 @@
-from gempy.utils.diamond_square import square
-import pytest # to add fixtures
-import numpy as np # as another testing environment
+import gempy.utils.diamond_square
+import pytest  # to add fixtures
+import numpy as np  # as another testing environment
 
 
-
-@pytest.fixture(scope="module")
-def data():
-    return [1,2,3,4]
-
+def test_class_nocrash():
+    """Simply check if class can be instantiated"""
+    gempy.utils.diamond_square.DiaomondSquare(size=(5, 5))
 
 
-def test_square_nocrash():
-    square([1])
+def test_grid_generation():
+    """Test grid generation and extension for non-suitable grid sizes"""
+    ds = gempy.utils.diamond_square.DiaomondSquare(size=(5, 5))
+    assert ds.grid.shape == (5, 5)
+    ds = gempy.utils.diamond_square.DiaomondSquare(size=(8, 10))
+    assert ds.grid.shape == (9, 17)
 
 
-def test_square(data):
-    # without "fixtures"
-    # vals = [1,2,3,4]
-    # with "fixtures"
-    vals = data
+def test_diamond_selection():
+    """Test selection of diamond positions"""
+    ds = gempy.utils.diamond_square.DiaomondSquare(size=(5, 5))
+    z = ds.get_selection_diamond(1)
+    assert np.all(z == np.array([[2, 0, 0, 0, 2],
+                                 [0, 0, 0, 0, 0],
+                                 [0, 0, 1, 0, 0],
+                                 [0, 0, 0, 0, 0],
+                                 [2, 0, 0, 0, 2]]))
+    z = ds.get_selection_diamond(0)
+    assert np.all(z == np.array([[2, 0, 2, 0, 2],
+                                 [0, 1, 0, 1, 0],
+                                 [2, 0, 2, 0, 2],
+                                 [0, 1, 0, 1, 0],
+                                 [2, 0, 2, 0, 2]]))
 
-    assert square(vals) == [1,4,9,16]
+
+def test_square_selection():
+    """Test selection of diamond positions"""
+    ds = gempy.utils.diamond_square.DiaomondSquare(size=(5, 5))
+    z = ds.get_selection_square(0)
+    assert np.all(z == np.array([[0, 0, 2, 0, 2, 0, 0],
+                                 [0, 2, 1, 2, 1, 2, 0],
+                                 [2, 1, 2, 1, 2, 1, 2],
+                                 [0, 2, 1, 2, 1, 2, 0],
+                                 [2, 1, 2, 1, 2, 1, 2],
+                                 [0, 2, 1, 2, 1, 2, 0],
+                                 [0, 0, 2, 0, 2, 0, 0]]))
+    z = ds.get_selection_square(1)
+    assert np.all(z == np.array([[0, 0, 0, 0, 2, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 2, 0, 1, 0, 2, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [2, 0, 1, 0, 2, 0, 1, 0, 2],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 2, 0, 1, 0, 2, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 2, 0, 0, 0, 0]]))
+
+
+def test_random_initialization():
+    """Test random initialization of corner points"""
+    ds = gempy.utils.diamond_square.DiaomondSquare(size=(5, 6), seed=52062)
+    ds.random_initialization()
+    m_pow_max = min(ds.n, ds.m)
+    step_size = int(2 ** m_pow_max)
+    print(step_size)
+    np.testing.assert_array_almost_equal(ds.grid[::step_size, ::step_size],
+                                         np.array([[0.35127005, 0.55476571, 0.93745213],
+                                                   [0.66668382, 0.85215985, 0.53222795]]))
