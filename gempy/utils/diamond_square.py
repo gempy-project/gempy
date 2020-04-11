7b5a77eb43baea7192a468e0404843894b512d64
Florian Wellmann <flohorovicic@gmail.com>
2020-04-11 21:31:10 +0200

    Implemented full diamond-square interpolation algorithm

--- gempy/utils/diamond_square.py
+++ gempy/utils/diamond_square.py
@@ -49,7 +49,7 @@ import matplotlib.pyplot as plt
 
 class DiaomondSquare(object):
 
-    def __init__(self, size=(16, 16), roughness=0.5, z_min=0, z_max=1, seed=87243):
+    def __init__(self, size=(16, 16), roughness=0.5, z_min=0, z_max=1, **kwds):
         """Implementation of vectorized Diaomnd-Square algorithm for random topography generation
 
         Args:
@@ -68,11 +68,89 @@ class DiaomondSquare(object):
         # size is chosen.
         self.n = np.ceil(np.log2(self.size[0] - 1)).astype('int8')
         self.m = np.ceil(np.log2(self.size[1] - 1)).astype('int8')
-        self.grid = np.empty((2 ** self.n + 1, 2 ** self.m + 1))
+        self.grid = np.zeros((2 ** self.n + 1, 2 ** self.m + 1))
         self.roughness = roughness
         self.z_min = z_min
         self.z_max = z_max
-        self.seed = seed
+        if 'seed' in kwds:
+            np.random.seed(kwds['seed'])
+
+    def interpolate(self):
+        """Perform diamond-square interpolation
+
+        This step follows the conventional procedure:
+        Iterate over hierarchies and repeat:
+        2) Perform Diamond interpolation step
+        3) Perform Square interpolation step
+        4) Reduce roughness factor
+        """
+        # determine highest hierarchy level (determined by shorter rectangle side)
+        m_pow_max = min(self.n, self.m)
+        for i, m_pow in enumerate(np.arange(m_pow_max)[::-1]):
+            print(m_pow)
+            self.perform_diamond_step(i, m_pow)
+            self.perform_square_step(i, m_pow)
+
+    def reset_grid(self):
+        """Reset grid back to zero values"""
+        self.grid[:, :] = 0
+
+    def perform_diamond_step(self, i, m_pow):
+        """Perform one diamond interpolation step on hierarchy m_pow
+
+        Note: for more details on the vectorized selection, see self.get_selection_diamond()
+        """
+        step_size = int(2 ** m_pow)
+
+        # Diamond step
+        # ----------------
+
+        self.grid[step_size::2 * step_size, step_size::2 * step_size] = \
+            (self.grid[:-2 * step_size:2 * step_size, :-2 * step_size:2 * step_size] +
+             self.grid[:-2 * step_size:2 * step_size, 2 * step_size::2 * step_size] +
+             self.grid[2 * step_size::2 * step_size, :-2 * step_size:2 * step_size] +
+             self.grid[2 * step_size::2 * step_size, 2 * step_size::2 * step_size]) / \
+            4. + random.random() ** i * self.roughness
+
+    def perform_square_step(self, i, m_pow):
+        """Perform one square interpolation step on hierarchy m_pow
+
+        Note: for more details on the vectorized selection, see self.get_selection_square()
+        """
+        step_size = int(2 ** m_pow)
+
+        # pad cells with zero value
+        z_pad = np.pad(self.grid, step_size)
+
+        # also create a grid for division to divide only by 3 on borders
+        grid_div = np.ones_like(self.grid[1:-1, 1:-1]) * 4.
+        grid_div = np.pad(grid_div, step_size+1, mode='constant', constant_values=3.)
+
+        # Checkerboard odd
+        # ----------------
+
+        z_pad[step_size::2 * step_size, 2 * step_size:-2 * step_size:2 * step_size] = \
+            (z_pad[step_size::2 * step_size, step_size:-2 * step_size:2 * step_size] +
+             z_pad[step_size::2 * step_size, 3 * step_size:-step_size:2 * step_size] +
+             z_pad[:-step_size:2 * step_size, 2 * step_size:-2 * step_size:2 * step_size] +
+             z_pad[2 * step_size::2 * step_size, 2 * step_size:-2 * step_size:2 * step_size]) / \
+            grid_div[step_size::2 * step_size, 2 * step_size:-2 * step_size:2 * step_size] +\
+            random.random() ** i * self.roughness
+
+        # Checkerboard even
+        # -----------------
+
+        # check-even, values to interpolate:
+        z_pad[2 * step_size:-2 * step_size:2 * step_size, step_size:-step_size:2 * step_size] = \
+            (z_pad[2 * step_size:-2 * step_size:2 * step_size, :-2 * step_size:2 * step_size] +
+             z_pad[2 * step_size:-2 * step_size:2 * step_size, 2 * step_size::2 * step_size] +
+             z_pad[step_size:-2 * step_size:2 * step_size, step_size:-step_size:2 * step_size] +
+             z_pad[3 * step_size::2 * step_size, step_size:-step_size:2 * step_size]) / \
+            grid_div[2 * step_size:-2 * step_size:2 * step_size, step_size:-step_size:2 * step_size] +\
+            random.random() ** i * self.roughness
+
+        # assign results back to self.grid
+        self.grid = z_pad[step_size:-step_size, step_size:-step_size]
 
     def get_selection_diamond(self, m_pow):
         """get selected points for diamond step on grid z on hierarchy m
@@ -80,24 +158,24 @@ class DiaomondSquare(object):
         This method is mostly implemented for testing and visualization purposes.
         """
 
-        m = int(2 ** m_pow)
+        step_size = int(2 ** m_pow)
 
-        z = np.zeros_like(self.grid)
+        z = np.zeros_like(self.grid, dtype='int8')
 
         # points to interpolate
-        z[m::2 * m, m::2 * m] = 1
+        z[step_size::2 * step_size, step_size::2 * step_size] = 1
 
         # top left
-        z[:-2 * m:2 * m, :-2 * m:2 * m] = 2
+        z[:-2 * step_size:2 * step_size, :-2 * step_size:2 * step_size] = 2
 
         # top right
-        z[:-2 * m:2 * m, 2 * m::2 * m] = 2
+        z[:-2 * step_size:2 * step_size, 2 * step_size::2 * step_size] = 2
 
         # bottom left
-        z[2 * m::2 * m, :-2 * m:2 * m] = 2
+        z[2 * step_size::2 * step_size, :-2 * step_size:2 * step_size] = 2
 
         # bottom right
-        z[2 * m::2 * m, 2 * m::2 * m] = 2
+        z[2 * step_size::2 * step_size, 2 * step_size::2 * step_size] = 2
 
         return z
 
@@ -106,47 +184,47 @@ class DiaomondSquare(object):
 
         This method is mostly implemented for testing and visualization purposes.
         """
-        z = np.zeros_like(self.grid)
-        m = int(2 ** m_pow)
+        z = np.zeros_like(self.grid, dtype='int8')
+        step_size = int(2 ** m_pow)
 
         # pad cells with zero value
-        z_pad = np.pad(z, m)
+        z_pad = np.pad(z, step_size)
 
         # Checkerboard odd
         # ----------------
 
         # check-odd, values to interpolate:
-        z_pad[m::2 * m, 2 * m:-2 * m:2 * m] = 1
+        z_pad[step_size::2 * step_size, 2 * step_size:-2 * step_size:2 * step_size] = 1
 
         # check-odd, left
-        z_pad[m::2 * m, m:-2 * m:2 * m] = 2
+        z_pad[step_size::2 * step_size, step_size:-2 * step_size:2 * step_size] = 2
 
         # check-odd, right
-        z_pad[m::2 * m, 3 * m:-m:2 * m] = 2
+        z_pad[step_size::2 * step_size, 3 * step_size:-step_size:2 * step_size] = 2
 
         # check-odd, top
-        z_pad[:-m:2 * m, 2 * m:-2 * m:2 * m] = 2
+        z_pad[:-step_size:2 * step_size, 2 * step_size:-2 * step_size:2 * step_size] = 2
 
         # check-odd, bottom
-        z_pad[2 * m::2 * m, 2 * m:-2 * m:2 * m] = 2
+        z_pad[2 * step_size::2 * step_size, 2 * step_size:-2 * step_size:2 * step_size] = 2
 
         # Checkerboard even
         # -----------------
 
         # check-even, values to interpolate:
-        z_pad[2 * m:-2 * m:2 * m, m:-m:2 * m] = 1
+        z_pad[2 * step_size:-2 * step_size:2 * step_size, step_size:-step_size:2 * step_size] = 1
 
         # check-even, left:
-        z_pad[2 * m:-2 * m:2 * m, :-2 * m:2 * m] = 2
+        z_pad[2 * step_size:-2 * step_size:2 * step_size, :-2 * step_size:2 * step_size] = 2
 
         # check-even, right:
-        z_pad[2 * m:-2 * m:2 * m, 2 * m::2 * m] = 2
+        z_pad[2 * step_size:-2 * step_size:2 * step_size, 2 * step_size::2 * step_size] = 2
 
         # check-even, top:
-        z_pad[m:-2 * m:2 * m, m:-m:2 * m] = 2
+        z_pad[step_size:-2 * step_size:2 * step_size, step_size:-step_size:2 * step_size] = 2
 
         # check-even, bottom:
-        z_pad[3 * m::2 * m, m:-m:2 * m] = 2
+        z_pad[3 * step_size::2 * step_size, step_size:-step_size:2 * step_size] = 2
 
         return z_pad
 
@@ -171,3 +249,13 @@ class DiaomondSquare(object):
             else:
                 axes[0, i].imshow(z_diamond, cmap='viridis', vmin=0, vmax=2)
                 axes[1, i].imshow(z_square[m:-m, m:-m], cmap='viridis', vmin=0, vmax=2)
+
+    def random_initialization(self):
+        """Initialize cells on highest hierarchy with random values
+
+        With highest hierarchy, we refer here to the largest diamond-square step, i.e. the corner points
+        for a square grid; Or, more formally: the diamond points for `min(self.n, self.m)`
+        """
+        m_pow_max = min(self.n, self.m)
+        step_size = int(2 ** m_pow_max)
+        self.grid[::step_size, ::step_size] = np.random.random((self.n, self.m))
