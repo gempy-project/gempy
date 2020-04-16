73e996e7ba637bb8131f7c393e60f02a46bc6994
Japhiolite <jan.niederau@rwth-aachen.de>
2020-04-16 17:56:08 +0200

    add new topo section function for testing and comparing with old one

--- gempy/core/grid_modules/grid_types.py
+++ gempy/core/grid_modules/grid_types.py
@@ -501,6 +501,19 @@ class Topography:
         return ((self.values_3D_res[:, :, 2] - zs[0]) / dz + 1).astype(int)
 
     def interpolate_zvals_at_xy(self, xy):
+        assert xy[:, 0][0] <= xy[:, 0][-1], 'At the moment, the xy values of the first point must be smaller than second' \
+                                            '(fix soon)'
+        assert xy[:, 1][0] <= xy[:, 1][-1], 'At the moment, the xy values of the first point must be smaller than second' \
+                                            '(fix soon)'
+        xj = self.values_3D[:, :, 0][0, :]
+        yj = self.values_3D[:, :, 1][:, 0]
+        zj = self.values_3D[:, :, 2].T
+
+        f = interpolate.RectBivariateSpline(xj, yj, zj)
+        zi = f(xy[:, 0], xy[:, 1])
+        return np.diag(zi)
+
+    def interpolate_zvals_at_xy_new(self, xy):
         #assert xy[:, 0][0] <= xy[:, 0][-1], 'At the moment, the xy values of the first point must be smaller than second' \
         #                                    '(fix soon)'
         #assert xy[:, 1][0] <= xy[:, 1][-1], 'At the moment, the xy values of the first point must be smaller than second' \
