b27d855ee23ced2ac97a1c344e05927d030a6176
Adam Kosík <adam.kosik@ost.ch>
2020-04-21 09:51:00 +0200

    Generate colordict with own palette.

--- gempy/core/data.py
+++ gempy/core/data.py
@@ -648,14 +648,29 @@ class Colors:
         self.surfaces = surfaces
 
     def generate_colordict(self, out = False):
-        import seaborn as sns
         """generate colordict that assigns black to faults and random colors to surfaces"""
-        gp_defcols = ['#015482','#9f0052','#ffbe00','#728f02','#443988','#ff3f20','#5DA629']
-        n_additional_colors=len(self.surfaces.df)-len(gp_defcols)
-        s = sns.color_palette(n_colors=n_additional_colors).as_hex()
-        gp_defcols += s
+        import seaborn as sns
+        # Creating own color palette
+        gp_palette = ['#015482', '#9f0052', '#ffbe00', '#728f02', '#443988',
+                      '#ff3f20', '#5DA629', '#b271d0', '#72e54a', '#583bd1',
+                      '#d0e63d', '#b949e2', '#95ce4b', '#6d2b9f', '#60eb91',
+                      '#d746be', '#52a22e', '#5e63d8', '#e5c339', '#371970',
+                      '#d3dc76', '#4d478e', '#43b665', '#d14897', '#59e5b8',
+                      '#e5421d', '#62dedb', '#df344e', '#9ce4a9', '#d94077',
+                      '#99c573', '#842f74', '#578131', '#708de7', '#df872f',
+                      '#5a73b1', '#ab912b', '#321f4d', '#e4bd7c', '#142932',
+                      '#cd4f30', '#69aedd', '#892a23', '#aad6de', '#5c1a34',
+                      '#cfddb4', '#381d29', '#5da37c', '#d8676e', '#52a2a3',
+                      '#9b405c', '#346542', '#de91c9', '#555719', '#bbaed6',
+                      '#945624', '#517c91', '#de8a68', '#3c4b64', '#9d8a4d',
+                      '#825f7e', '#2c3821', '#ddadaa', '#5e3524', '#a3a68e',
+                      '#a2706b', '#686d56']
+        # Modify it to the number of surfaces with a cycle of defined colors
+        n_surf = len(self.surfaces.df)
+        gp_defcols = sns.color_palette(gp_palette, n_colors=n_surf).as_hex()
 
-        colordict = dict(zip(list(self.surfaces.df['surface']), gp_defcols[:len(self.surfaces.df)]))
+
+        colordict = dict(zip(list(self.surfaces.df['surface']), gp_defcols))
         self.colordict_default = colordict
         if out:
             return colordict
