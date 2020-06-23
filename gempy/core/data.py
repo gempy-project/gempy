2bd2575ce0866ace8a7fbd1825b1b54e87baf5f6
Alex <a.schaaf@abdn.ac.uk>
2020-06-23 10:39:24 +0200

    Refactoring Color class methods for clarity

--- gempy/core/data.py
+++ gempy/core/data.py
@@ -284,9 +284,13 @@ class Colors:
 
     def __init__(self, surfaces):
         self.surfaces = surfaces
+        self.colordict = None
 
-    def generate_colordict(self, hex_colors='palettes',
-                           palettes: List[str] = 'default', out=False):
+    def generate_colordict(
+            self,
+            hex_colors: Union[List[str], str] = 'palettes',
+            palettes: List[str] = 'default',
+    ):
         """generate colordict that assigns black to faults and random colors to surfaces
 
         Args:
@@ -304,33 +308,38 @@ class Colors:
         if hex_colors == 'palettes':
             hex_colors = []
             if palettes == 'default':
+                # we predefine some 7 colors manually
                 hex_colors = ['#015482', '#9f0052', '#ffbe00', '#728f02', '#443988', '#ff3f20', '#5DA629']
+
+                # then we create a list of seaborn color palette names, as the user didn't provide any
                 palettes = ['muted', 'pastel', 'deep', 'bright', 'dark', 'colorblind']
 
-            for i in palettes:
-                s = sns.color_palette(i).as_hex()
-                hex_colors += s
+            for palette in palettes:  # for each palette
+                hex_colors += sns.color_palette(palette).as_hex()
                 if len(hex_colors) >= len(self.surfaces.df):
                     break
         elif hex_colors == 'soft':
-            hex_colors = ['#015482', '#9f0052', '#ffbe00', '#728f02', '#443988',
-                          '#ff3f20', '#5DA629', '#b271d0', '#72e54a', '#583bd1',
-                          '#d0e63d', '#b949e2', '#95ce4b', '#6d2b9f', '#60eb91',
-                          '#d746be', '#52a22e', '#5e63d8', '#e5c339', '#371970',
-                          '#d3dc76', '#4d478e', '#43b665', '#d14897', '#59e5b8',
-                          '#e5421d', '#62dedb', '#df344e', '#9ce4a9', '#d94077',
-                          '#99c573', '#842f74', '#578131', '#708de7', '#df872f',
-                          '#5a73b1', '#ab912b', '#321f4d', '#e4bd7c', '#142932',
-                          '#cd4f30', '#69aedd', '#892a23', '#aad6de', '#5c1a34',
-                          '#cfddb4', '#381d29', '#5da37c', '#d8676e', '#52a2a3',
-                          '#9b405c', '#346542', '#de91c9', '#555719', '#bbaed6',
-                          '#945624', '#517c91', '#de8a68', '#3c4b64', '#9d8a4d',
-                          '#825f7e', '#2c3821', '#ddadaa', '#5e3524', '#a3a68e',
-                          '#a2706b', '#686d56']
+            hex_colors = [
+                '#015482', '#9f0052', '#ffbe00', '#728f02', '#443988',
+                '#ff3f20', '#5DA629', '#b271d0', '#72e54a', '#583bd1',
+                '#d0e63d', '#b949e2', '#95ce4b', '#6d2b9f', '#60eb91',
+                '#d746be', '#52a22e', '#5e63d8', '#e5c339', '#371970',
+                '#d3dc76', '#4d478e', '#43b665', '#d14897', '#59e5b8',
+                '#e5421d', '#62dedb', '#df344e', '#9ce4a9', '#d94077',
+                '#99c573', '#842f74', '#578131', '#708de7', '#df872f',
+                '#5a73b1', '#ab912b', '#321f4d', '#e4bd7c', '#142932',
+                '#cd4f30', '#69aedd', '#892a23', '#aad6de', '#5c1a34',
+                '#cfddb4', '#381d29', '#5da37c', '#d8676e', '#52a2a3',
+                '#9b405c', '#346542', '#de91c9', '#555719', '#bbaed6',
+                '#945624', '#517c91', '#de8a68', '#3c4b64', '#9d8a4d',
+                '#825f7e', '#2c3821', '#ddadaa', '#5e3524', '#a3a68e',
+                '#a2706b', '#686d56'
+            ]
 
-        colordict = dict(zip(list(self.surfaces.df['surface']), hex_colors[:len(self.surfaces.df)]))
-        self.colordict = colordict
-        return colordict
+        # create color dict based on hex_colors list of hex strings (either provided or generated above)
+        self.colordict = dict(
+            zip(self.surfaces.df['surface'].values, hex_colors[:len(self.surfaces.df)])
+        )
 
     def change_colors(self, cdict=None):
         ''' Updates the colors of the model.
@@ -362,22 +371,22 @@ class Colors:
             for cols in colbox.children:
                 cols.observe(on_change, 'value')
 
-    def update_colors(self, cdict=None):
+    def update_colors(self, colordict: dict = None):
         ''' Updates the colors in self.colordict and in surfaces_df.
         Args:
-            cdict: dict with surface names mapped to hex color codes, e.g. {'layer1':'#6b0318'}
+            colordict: dict with surface names mapped to hex color codes, e.g. {'layer1':'#6b0318'}
 
         Returns: None
 
         '''
-        if cdict is None:
-            # assert if one surface does not have color
-            try:
-                self._add_colors()
+        if colordict is None:
+            try:  # assert if any surface does not have a color
+                self._add_colors()  # the first thing this function does is call the same function as in the
+                # exception handling???
             except AttributeError:
                 self.generate_colordict()
         else:
-            for surf, color in cdict.items():  # map new colors to surfaces
+            for surf, color in colordict.items():  # map new colors to surfaces
                 # assert this because user can set it manually
                 assert surf in list(self.surfaces.df['surface']), str(surf) + ' is not a model surface'
                 assert re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', color), str(color) + ' is not a HEX color code'
@@ -388,10 +397,10 @@ class Colors:
     def _add_colors(self):
         '''assign color to last entry of surfaces df or check isnull and assign color there'''
         # can be done easier
-        new_colors = self.generate_colordict()
+        self.generate_colordict()
         form2col = list(self.surfaces.df.loc[self.surfaces.df['color'].isnull(), 'surface'])
         # this is the dict in-build function to update colors
-        self.colordict.update(dict(zip(form2col, [new_colors[x] for x in form2col])))
+        self.colordict.update(dict(zip(form2col, [self.colordict[x] for x in form2col])))
 
     def _set_colors(self):
         '''sets colordict in surfaces dataframe'''
