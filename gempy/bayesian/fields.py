2f5db1eb1b71642f2a1dbbdee268a8f75027f167
Andrew Annex <andrew.annex@gmail.com>
2020-09-30 17:00:35 -0400

    Expand/refactor information/uncertainty functions
    refactors and expands the probablities and information entropy
    functions
    now includes total model entropy and fuzziness from the 2012 "Uncertainties
    have a meaning" paper.
    Modest speedup for probability function, uses scipy for information
    entropy calculation enabling base change.

--- gempy/bayesian/fields.py
+++ gempy/bayesian/fields.py
@@ -1,20 +1,64 @@
 import numpy as np
 
+from scipy.stats import entropy
+import warnings
+
 
 def compute_prob(blocks):
+    warnings.warn("This function is Deprecated, please use the probability function instead")
+    return probability(blocks)
+
+
+def probability(blocks: np.ndarray) -> np.ndarray:
+    """
+    compute the probabilities for the unique values in the array given
+    :param blocks: numpy array of different computed model results
+    :return:
+    """
     blocks = np.round(blocks)
-    lith_id = np.unique(blocks)
-    # lith_count = np.zeros_like(lith_blocks[0:len(lith_id)])
-    count = np.zeros((len(np.unique(blocks)), blocks.shape[1]))
-    for i, l_id in enumerate(lith_id):
-        count[i] = np.sum(blocks == l_id, axis=0)
-    prob = count / len(blocks)
-    return prob
+    return np.mean([blocks == _ for _ in np.unique(blocks)], axis=1)
 
 
 def calculate_ie_masked(prob):
-    ie = np.zeros_like(prob[0])
-    for l in prob:
-        pm = np.ma.masked_equal(l, 0)  # mask where prob is 0
-        ie -= (pm * np.ma.log2(pm)).filled(0)
-    return ie
+    warnings.warn("This function is Deprecated, please use the information_entropy function instead")
+    return information_entropy(prob)
+
+
+def information_entropy(probabilities: np.ndarray, base=2) -> np.ndarray:
+    """
+    Calculate the Information Entropy of the provided probabilities array
+
+    Eq 2. from doi.org/10.1016/j.tecto.2011.05.001
+
+    but using scipy implementation for speed and simplicity
+
+    note the paper assumes base two everywhere
+
+    :param base: base for entropy, defaults to 2 as per paper
+    :param probabilities: probabilities array, first axis is for each possible layers (M, n cells)
+    :return: entropy of the probability array, flattens the first dimension
+    """
+    return entropy(probabilities, base=base)
+
+
+def fuzziness(probabilities: np.ndarray) -> float:
+    """
+    Return the fuzziness of the probability array
+
+    Eq 3. from doi.org/10.1016/j.tecto.2011.05.001
+    :param probabilities: probabilities array
+    :return: float of fuzziness
+    """
+    p = probabilities
+    fuzz = -np.mean(np.nan_to_num(p * np.log(p) + (1 - p) * np.log(1 - p)))
+    return fuzz
+
+
+def total_model_entropy(ie: np.ndarray) -> float:
+    """
+    Return the Total Model Information Entropy (the mean of the array)
+    Eq 4. from doi.org/10.1016/j.tecto.2011.05.001
+    :param ie: information entropy array
+    :return:
+    """
+    return float(np.mean(ie))
