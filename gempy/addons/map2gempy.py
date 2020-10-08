ee30df9e437393e76045393292eb3bcf27279347
Miguel de la Varga <miguel@terranigma-solutions.com>
2020-10-08 16:35:48 +0200

    - [ENH] Added map2gempy function

---
+++ gempy/addons/map2gempy.py
@@ -0,0 +1,146 @@
+import gempy as gp
+import pandas as pd
+import numpy as np
+
+
+def loop2gempy(
+        contacts_file: str,
+        orientations_file: str,
+        bbox: tuple,
+        groups_file: str,
+        model_base: float,
+        model_top: float, dtm_reproj_file: str = None,
+        faults_contact: str = None,
+        faults_orientations: str = None,
+        model_name: str = None,
+        compute: bool = True,
+        vtk: bool = False,
+        vtk_path: str = None,
+
+):
+
+    contacts = []
+    orientations = []
+
+    contacts.append(
+        pd.read_csv(
+            contacts_file,
+            sep=',',
+            names=['X', 'Y', 'Z', 'formation'],
+            header=1
+        )
+    )
+
+    if faults_contact is not None:
+        contacts.append(
+            pd.read_csv(
+                faults_contact,
+                sep=',',
+                names=['X', 'Y', 'Z', 'formation'],
+                header=1
+            )
+        )
+
+    orientations.append(
+        pd.read_csv(
+            orientations_file,
+            sep=',',
+            names=['X', 'Y', 'Z', 'azimuth', 'dip', 'polarity', 'formation'],
+            header=1
+        )
+    )
+
+    if faults_orientations is not None:
+        orientations.append(
+            pd.read_csv(
+                faults_orientations,
+                sep=',',
+                names=['X', 'Y', 'Z', 'azimuth', 'dip', 'polarity', 'formation'],
+                header=1
+            )
+        )
+
+    surface_points_ready = pd.concat(contacts, sort=True)
+    surface_points_ready.reset_index(inplace=True, drop=False)
+    orientation_ready = pd.concat(orientations, sort=True)
+    orientation_ready.reset_index(inplace=True, drop=False)
+
+    if model_name is None:
+        model_name = 'loop2gempy'
+
+    geo_model = gp.create_model(model_name)
+    gp.init_data(
+        geo_model,
+        extent=[bbox[0], bbox[2], bbox[1], bbox[3], model_base, model_top],
+        resolution=[50, 50, 50],
+        orientations_df=orientation_ready,
+        surface_points_df=surface_points_ready
+    )
+
+    # Load Topology
+    if dtm_reproj_file is not None:
+        geo_model.set_topography(source='gdal', filepath=dtm_reproj_file)
+
+    # Stack Processing
+    contents = np.genfromtxt(groups_file, delimiter=',', dtype='U100')[1:, 4:-1]
+
+    map_series_to_surfaces = {}
+    for pair in contents:
+        map_series_to_surfaces.setdefault(pair[1], []).append(pair[0])
+
+    gp.map_stack_to_surfaces(geo_model, map_series_to_surfaces,
+                             remove_unused_series=False)
+
+    order_formations = geo_model.stack.df.index.drop('Default series')
+
+    # Get the unassigned series as faults
+    if faults_contact is not None and faults_orientations is not None:
+        faults_pair = geo_model._surfaces.df.groupby('series').get_group('Default series')[[
+            'surface']].values[:, 0]
+        faults_pair_dict = dict(zip(faults_pair, faults_pair))
+        gp.map_stack_to_surfaces(geo_model, faults_pair_dict, remove_unused_series=True)
+        # Grabbing the order of faults and Formations
+        ordered_features = np.append(faults_pair, order_formations)
+        # Sorting series
+        geo_model.reorder_features(ordered_features)
+        geo_model.set_is_fault(faults_pair)
+
+    geo_model.add_surfaces('basement')
+
+    # Check if there is features without data and delete it
+    try:
+        f_ = geo_model.surfaces.df.groupby('isActive').get_group(False)
+        features_without_data = f_['series']
+        geo_model.delete_features(features_without_data, remove_surfaces=True, remove_data=True)
+    except KeyError:
+        pass
+
+    if faults_contact is not None and faults_orientations is not None:
+        get_fault_names = geo_model.stack.df.groupby(['isActive', 'isFault']).get_group(
+            (True, True)).index
+        geo_model._surfaces.colors.make_faults_black(get_fault_names)
+
+    if compute is True:
+        gp.set_interpolator(geo_model)
+
+        # Increasing nugget effect
+        geo_model.modify_surface_points(
+            geo_model.surface_points.df.index,
+            smooth=0.1
+        )
+        new_range = geo_model.get_additional_data().loc[('Kriging', 'range'), 'values'] * 0.3
+        geo_model.modify_kriging_parameters('range', new_range)
+
+        gp.compute_model(geo_model)
+
+    if vtk is True:
+        gp.plot_3d(geo_model, ve=10, show_topography=True,
+                   image=False,
+                   show_lith=False,
+                   )
+
+    if vtk_path is not None:
+        gp._plot.export_to_vtk(geo_model, path=vtk_path, name=model_name + '.vtk',
+                               voxels=False, block=None, surfaces=True)
+
+    return geo_model
--- test/test_integrations/test_map2loop.py
+++ test/test_integrations/test_map2loop.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Input files
+from gempy.addons.map2gempy import loop2gempy
+
 root = 'https://raw.githubusercontent.com/cgre-aachen/gempy_data/master/data/input_data/turner_syncline/'
 
 orientations_file = root + 'orientations_clean.csv'
@@ -18,8 +20,14 @@ faults_rel_matrix = root + 'fault-fault-relationships.csv'
 series_rel_matrix = root + 'group-fault-relationships.csv'
 
 bbox = (500000, 7490000, 545000, 7520000)
-model_base = -1500  # Original 3200
+model_base = -0  # Original 3200
 model_top = 800
+
+
+def test_loo2gempy():
+    loop2gempy(contacts_file, orientations_file, bbox, series_file, model_base,
+               model_top, fp, faults_contact, faults_orientations, 'testing_map',
+               vtk=True, vtk_path='./')
 
 
 def test_map2loop_model_input_data_preparation():
@@ -76,9 +84,13 @@ def test_map2loop_model_input_data_preparation():
         surface_points_df=surface_points_ready
     )
 
+    # Load Topology
+    topo = True
+    if topo:
+        geo_model.set_topography(source='gdal', filepath=fp)
+
     # Stack Processing
-    contents = np.genfromtxt(series_file,
-                             delimiter=',', dtype='U100')[1:, 4:-1]
+    contents = np.genfromtxt(series_file, delimiter=',', dtype='U100')[1:, 4:-1]
 
     map_series_to_surfaces = {}
     for pair in contents:
@@ -87,18 +99,38 @@ def test_map2loop_model_input_data_preparation():
     gp.map_stack_to_surfaces(geo_model, map_series_to_surfaces,
                              remove_unused_series=False)
 
+    order_formations = geo_model.stack.df.index.drop('Default series')
+
     # Get the unassigned series
     faults_pair = geo_model._surfaces.df.groupby('series').get_group('Default series')[[
         'surface']].values[:, 0]
     faults_pair_dict = dict(zip(faults_pair, faults_pair))
-    gp.map_stack_to_surfaces(geo_model, faults_pair_dict)
+    gp.map_stack_to_surfaces(geo_model, faults_pair_dict, remove_unused_series=True)
 
     # Grabbing the order of faults and Formations
 
     # Sorting series
 
-    ordered_features = np.append()
-    geo_model.reorder_features([faults_pair])
+
+    ordered_features = np.append(faults_pair, order_formations)
+    geo_model.reorder_features(ordered_features)
+
+    geo_model.set_is_fault(faults_pair)
+    geo_model.add_surfaces('basement')
+
+    try:
+        f_ = geo_model.surfaces.df.groupby('isActive').get_group(False)
+        features_without_data = f_['series']
+        geo_model.delete_features(features_without_data, remove_surfaces=True, remove_data=True)
+    except KeyError:
+        pass
+
+    get_fault_names = geo_model.stack.df.groupby(['isActive', 'isFault']).get_group((True, True)).index
+    geo_model._surfaces.colors.make_faults_black(get_fault_names)
+    # geo_model.set_is_finite_fault(faults_pair[1:])
+    compute_and_plot(geo_model)
+    print(geo_model)
+
 
 def test_map2loop_model_import_data():
     geo_model = gp.create_model('test_map2Loop')
@@ -138,6 +170,8 @@ def test_map2loop_model():
         path_o=orientations_file,
         path_i=contacts_file
     )
+
+    gp.set_interpolator(geo_model)
 
     # Load Topology
     geo_model.set_topography(source='gdal', filepath=fp)
@@ -186,6 +220,33 @@ def test_map2loop_model():
                section_names=['topography'],
                show_topography=True,
                )
+    plt.show()
+
+    gp.plot_3d(geo_model, ve=10, show_topography=True,
+               image=False,
+               show_lith=False,
+               )
+
+
+def compute_and_plot(geo_model):
+    gp.set_interpolator(geo_model)
+    # Adding axial rescale
+   # geo_model._rescaling.toggle_axial_anisotropy(type='data')
+
+    # Increasing nugget effect
+    geo_model.modify_surface_points(
+        geo_model.surface_points.df.index,
+        smooth=0.1
+    )
+    new_range = geo_model.get_additional_data().loc[('Kriging', 'range'), 'values'] * 0.3
+    geo_model.modify_kriging_parameters('range', new_range)
+
+    gp.compute_model(geo_model)
+
+    #gp.plot_2d(geo_model,
+    #           section_names=['topography'],
+               #show_topography=True,
+    #           )
     plt.show()
 
     gp.plot_3d(geo_model, ve=10, show_topography=True,
