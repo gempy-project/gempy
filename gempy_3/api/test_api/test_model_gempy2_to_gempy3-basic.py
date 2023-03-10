c0500696271bbff662448c1e705eaf3b7fef688b
miguel <miguel@terranigma-solutions.com>
2023-03-10 10:42:46 +0100

    [WIP] preparing test for nugget effect

--- gempy_3/api/test_api/test_model_gempy2_to_gempy3-basic.py
+++ gempy_3/api/test_api/test_model_gempy2_to_gempy3-basic.py
@@ -5,6 +5,7 @@ from gempy import Project
 from gempy.plot.vista import GemPyToVista
 from gempy_3.api.gp2_to_gp3_input import gempy_project_to_interpolation_input, gempy_project_to_input_data_descriptor, gempy_project_to_interpolation_options
 from gempy_3.api.gp3_to_gp2_output import set_gp3_solutions_to_gp2_solution
+from gempy_3.api.test_api._gp2togp3_test_utils import create_interpolator, load_model, map_sequential_pile
 from gempy_engine.config import AvailableBackends
 from gempy_engine.core.backend_tensor import BackendTensor
 from gempy_engine.core.data import InterpolationOptions
@@ -16,33 +17,6 @@ input_path = os.path.dirname(__file__) + '/../../../test/input_data'
 import sys  # These two lines are necessary only if GemPy is not installed
 
 sys.path.append("../..")
-
-
-def create_interpolator():
-    m = gp.create_model('JustInterpolator')
-    return gp.set_interpolator(m, theano_optimizer='fast_compile')
-
-
-def load_model():
-    geo_model = gp.create_model('Model_Tuto1-1')
-    # Importing the data from CSV-files and setting extent and resolution
-    gp.init_data(geo_model, [0, 2000., 0, 2000., 0, 2000.], [50, 50, 50],
-                 path_o=input_path + "/simple_fault_model_orientations.csv",
-                 path_i=input_path + "/simple_fault_model_points.csv", default_values=True)
-    return geo_model
-
-
-def map_sequential_pile(geo_model: Project) -> Project:
-    gp.map_stack_to_surfaces(geo_model=geo_model,
-                             mapping_object={
-                                 "Fault_Series": 'Main_Fault',
-                                 "Strat_Series": ('Sandstone_2', 'Siltstone',
-                                                  'Shale', 'Sandstone_1', 'basement')
-                             },
-                             remove_unused_series=True)
-
-    geo_model.set_is_fault(['Fault_Series'])
-    return geo_model
 
 
 def test_set_gempy3_gempy2_bridge():
