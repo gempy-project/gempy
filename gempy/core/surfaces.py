7868b7ebd9e4ecf62e44e284fdb48cb75fcfd005
Niederau <jan.niederau@rwth-aachen.de>
2023-05-24 15:43:15 +0200

    [WIP] change mapping surfaces to series

--- gempy/core/surfaces.py
+++ gempy/core/surfaces.py
@@ -328,6 +328,9 @@ class Surfaces(object):
 
                 new_series_mapping = pn.DataFrame([pn.Categorical(s, self.series.df.index)],
                                                   f, columns=['series'])
+                # with pandas 2.0.1 something along the lines of
+                new_series_mapping = pn.DataFrame(list(zip(pn.Categorical(s, self.series.df.index),
+                                                           f)), columns=['series'])
 
             elif isinstance(mapping_object, pn.Categorical):
                 # This condition is for the case we have surface on the index and in 'series' the category
