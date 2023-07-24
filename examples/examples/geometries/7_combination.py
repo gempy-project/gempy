5fdce50ae420385916b3194f1ce9680b4929d844
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-07-24 13:29:13 +0200

    - [WIP/ENH] Tweaking example 7

--- examples/examples/geometries/7_combination.py
+++ examples/examples/geometries/7_combination.py
@@ -3,6 +3,7 @@ Model 7 - Combination
 ======================
 
 """
+import numpy as np
 
 # %%
 # A folded domain featuring an unconformity and a fault. We start by importing
@@ -12,9 +13,8 @@ Model 7 - Combination
 # %%
 # Importing GemPy
 import gempy as gp
-
-import pandas as pd
-pd.set_option('display.precision', 2)
+import gempy_viewer as gpv
+from gempy_engine.core.data.stack_relation_type import StackRelationType
 
 # %%
 # Creating the model by importing the input data and displaying it:
@@ -24,35 +24,42 @@ pd.set_option('display.precision', 2)
 data_path = 'https://raw.githubusercontent.com/cgre-aachen/gempy_data/master/'
 path_to_data = data_path + "/data/input_data/jan_models/"
 
-geo_data = gp.create_data('combination',
-                          extent=[0, 2500, 0, 1000, 0, 1000],
-                          resolution=[125, 50, 50],
-                          path_o=path_to_data + "model7_orientations.csv",
-                          path_i=path_to_data + "model7_surface_points.csv")
-
-# %%
-geo_data.get_data()
+geo_data = gp.create_data(
+    project_name='combination',
+    extent=[0, 2500, 0, 1000, 0, 1000],
+    resolution=[125, 50, 50],
+    path_o=path_to_data + "model7_orientations.csv",
+    path_i=path_to_data + "model7_surface_points.csv"
+)
 
 # %%
 # Setting and ordering the units and series:
 #
 
 # %%
-gp.map_stack_to_surfaces(geo_data, {"Fault_Series": ('fault'), "Strat_Series1": ('rock3'),
-                                     "Strat_Series2": ('rock2','rock1'),
-                                     "Basement_Series":('basement')})
+gp.map_stack_to_surfaces(
+    gempy_model=geo_data,
+    mapping_object={
+        "Fault_Series" : ('fault'),
+        "Strat_Series1": ('rock3'),
+        "Strat_Series2": ('rock2', 'rock1'),
+    }
+)
 
-geo_data.set_is_fault(['Fault_Series'])
+geo_data.structural_frame.structural_groups[0].structural_relation = StackRelationType.FAULT
+geo_data.structural_frame.fault_relations = np.array(
+    [[0, 1, 1],
+     [0, 0, 0],
+     [0, 0, 0]]
+)
+
 
 # %%
-gp.plot_2d(geo_data, direction='y')
+gpv.plot_2d(geo_data, direction='y')
 
 # %%
 # Calculating the model:
 #
-
-# %%
-interp_data = gp.set_interpolator(geo_data, aesara_optimizer='fast_compile')
 
 # %%
 sol = gp.compute_model(geo_data)
@@ -62,14 +69,12 @@ sol = gp.compute_model(geo_data)
 #
 
 # %%
-gp.plot_2d(geo_data, cell_number=5,
-           direction='y', show_data=False, show_boundaries=True)
+gpv.plot_2d(geo_data, cell_number=5, direction='y', show_data=True, show_boundaries=True)
 
 # %%
 # sphinx_gallery_thumbnail_number = 2
-gp.plot_2d(geo_data, cell_number=5,
-           direction='x', show_data=True)
+gpv.plot_2d(geo_data, cell_number=5, direction='x', show_data=True)
 
 # %%
-gp.plot_3d(geo_data)
-#gp.save_model(geo_data)
+# BUG: Revive 3D plotting
+# gpv.plot_3d(geo_data)
