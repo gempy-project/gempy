95d359efcdc7e9d9206b6f3632ebc8983c126421
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-07-26 16:16:45 +0200

    - [ENH] Test example models

---
+++ test/test_model_types/test_example_models/test_example_models_I.py
@@ -0,0 +1,43 @@
+﻿import gempy as gp
+from examples.examples.geometries.a01_horizontal_stratigraphic import generate_horizontal_stratigraphic_model
+from examples.examples.geometries.b02_fold import generate_anticline_model
+from examples.examples.geometries.c03_recumbent_fold import generate_recumbent_fold_model
+from examples.examples.geometries.d04_pinchout import generate_pinchout_model
+from examples.examples.geometries.e05_fault import generate_fault_model
+from examples.examples.geometries.f06_unconformity import generate_unconformity_model
+from examples.examples.geometries.g07_combination import generate_combination_model
+
+
+def test_generate_horizontal_stratigraphic_model():
+    model = generate_horizontal_stratigraphic_model()
+    assert isinstance(model, gp.GeoModel)
+
+
+def test_generate_fold_model():
+    model = generate_anticline_model()
+    assert isinstance(model, gp.GeoModel)
+
+
+def test_generate_recumbent_fold_model():
+    model = generate_recumbent_fold_model()
+    assert isinstance(model, gp.GeoModel)
+
+
+def test_generate_pinchout_model():
+    model = generate_pinchout_model()
+    assert isinstance(model, gp.GeoModel)
+
+
+def test_generate_fault_model():
+    model = generate_fault_model()
+    assert isinstance(model, gp.GeoModel)
+
+
+def test_generate_unconformity_model():
+    model = generate_unconformity_model()
+    assert isinstance(model, gp.GeoModel)
+
+
+def test_generate_combination_model():
+    model = generate_combination_model()
+    assert isinstance(model, gp.GeoModel)
