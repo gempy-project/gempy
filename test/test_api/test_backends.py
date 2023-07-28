94120c2690c0fb6d263b1a3adeff12e510abea0b
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-07-28 10:49:13 +0200

    - [BUG] Weird breaking point when pykeops was not installed

--- test/test_api/test_backends.py
+++ test/test_api/test_backends.py
@@ -2,10 +2,9 @@
 
 import gempy as gp
 import gempy_viewer as gpv
+from gempy.core.data.gempy_engine_config import GempyEngineConfig
 from gempy_engine.config import AvailableBackends
 from gempy.core.data.enumerators import ExampleModel
-from gempy_engine.modules.octrees_topology.octrees_topology_interface import ValueType
-from plugins.plotting.helper_functions import plot_block_and_input_2d
 from test.conftest import TEST_SPEED
 
 
@@ -21,7 +20,7 @@ class TestBackends:
         return geo_model
     
     def test_backends_numpy(self, geo_model):
-        gp.compute_model(geo_model, backend=AvailableBackends.numpy)
+        gp.compute_model(geo_model, engine_config=GempyEngineConfig(backend=AvailableBackends.numpy))
         gpv.plot_3d(
             model=geo_model,
             show_data=True,
@@ -29,11 +28,29 @@ class TestBackends:
             show_lith=False,
             image=True
         )
+    
+    
+    def test_numpy_pykeops(self, geo_model):
+        gp.compute_model(
+            gempy_model=geo_model,
+            engine_config=GempyEngineConfig(
+                backend=AvailableBackends.numpy,
+                use_gpu=False,
+                pykeops_enabled=True
+            ))
         
+        gpv.plot_3d(
+            model=geo_model,
+            show_data=True,
+            show_boundaries=True,
+            show_lith=False,
+            image=True
+        )
+
     @pytest.mark.skip(reason="Not finished yet. Fault data is not passed to legacy properly.")
     @pytest.mark.skipif(TEST_SPEED.value <= 2, reason="Global test speed below this test value.")
     def test_backends_legacy(self, geo_model):
-        gp.compute_model(geo_model, backend=AvailableBackends.legacy)
+        gp.compute_model(geo_model, engine_config=GempyEngineConfig(backend=AvailableBackends.legacy))
         from gempy.optional_dependencies import require_gempy_legacy
         gpl = require_gempy_legacy()
         gpl.plot_3d(
