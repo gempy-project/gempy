db5d5bd5f6bc486856f58e47a40f04f4c7802097
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-07-31 14:37:37 +0200

    - [ENH] Implement unset_is_fault

--- gempy/API/faults_API.py
+++ gempy/API/faults_API.py
@@ -6,33 +6,44 @@ from gempy import GeoModel, StructuralFrame
 from gempy.core.data.structural_group import StructuralGroup, FaultsRelationSpecialCase
 
 
-def set_is_fault(frame: Union[GeoModel, StructuralFrame], feature_fault: Union[list[str], list[StructuralGroup]] = None,
-                 toggle: bool = False, change_color: bool = True) -> StructuralFrame:
-    # * Find the groups passed and set structural relation to fault
+def set_is_fault(frame: Union[GeoModel, StructuralFrame], feature_fault: Union[list[str], list[StructuralGroup]],
+                 change_color: bool = True) -> StructuralFrame:
     if isinstance(frame, GeoModel):
         frame = frame.structural_frame
-    
-    for index, group in enumerate(feature_fault):
-        if isinstance(group, str):
-            group = next((g for g in frame.structural_groups if g.name == group), None)
-        if isinstance(group, StructuralGroup):
-            group.structural_relation = StackRelationType.FAULT
-            group.fault_relations = FaultsRelationSpecialCase.OFFSET_ALL  # * Set the default fault relations
-        else:
-            raise ValueError(f"Could not find group '{group}' in structural frame.")
-        
-    
+
+    frame = _find_and_set_fields(
+        frame=frame,
+        feature_fault=feature_fault,
+        faults_relation_type=FaultsRelationSpecialCase.OFFSET_ALL,
+        stack_relation_type=StackRelationType.FAULT
+    )
+
     # * TODO: Set the fault colors
-    
+
     return frame
-    
-    
+
+
+def unset_is_fault(frame: Union[GeoModel, StructuralFrame], feature_fault: Union[list[str], list[StructuralGroup]],
+                   change_color: bool = True) -> StructuralFrame:
+    if isinstance(frame, GeoModel):
+        frame = frame.structural_frame
+
+    frame = _find_and_set_fields(
+        frame=frame,
+        feature_fault=feature_fault,
+        faults_relation_type=FaultsRelationSpecialCase.OFFSET_NONE,
+        stack_relation_type=StackRelationType.ERODE
+    )
+
+    return frame
+
+
 def set_fault_relation(frame: Union[GeoModel, StructuralFrame], rel_matrix: np.ndarray) -> StructuralFrame:
     """"""
     if isinstance(frame, GeoModel):
         frame = frame.structural_frame
-    
-    frame.fault_relations = rel_matrix 
+
+    frame.fault_relations = rel_matrix
     return frame
 
 
@@ -46,3 +57,14 @@ def set_is_finite_fault(self, series_fault=None, toggle: bool = True):
     return s
 
 
+def _find_and_set_fields(frame: StructuralFrame, feature_fault: list[StructuralGroup],
+                         faults_relation_type: FaultsRelationSpecialCase, stack_relation_type: StackRelationType) -> StructuralFrame:
+    for index, group in enumerate(feature_fault):
+        if isinstance(group, str):
+            group = next((g for g in frame.structural_groups if g.name == group), None)
+        if isinstance(group, StructuralGroup):
+            group.structural_relation = stack_relation_type
+            group.fault_relations = faults_relation_type  # * Set the default fault relations
+        else:
+            raise ValueError(f"Could not find group '{group}' in structural frame.")
+    return frame
