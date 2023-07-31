a8827a227b3129b6d6df1db6573428d23d82f487
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-07-31 11:33:14 +0200

    - [WIP/CLN] Preparing faults API

--- gempy/API/__init__.py
+++ gempy/API/__init__.py
@@ -1,5 +1,5 @@
 from .initialization_API import create_data_legacy, create_geomodel
 from .compute_API import compute_model
-from .mutation_API import map_stack_to_surfaces
+from .map_stack_to_surfaces_API import map_stack_to_surfaces
 from .grid_API import set_section_grid, set_active_grid, set_topography_from_random
 from .examples_generator import generate_example_model
