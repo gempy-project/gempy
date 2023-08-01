73b46533f46df0c443e9e1dd542585d704ddf0a5
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-01 14:00:05 +0200

    - [WIP] Trying to plot geological map

--- test/test_api/test_grids/test_grids_sections.py
+++ test/test_api/test_grids/test_grids_sections.py
@@ -15,29 +15,32 @@ def test_section_grids():
 
     geo_model.interpolation_options.number_octree_levels = 2
 
-    # gp.set_section_grid(
-    #     grid=geo_model.grid,
-    #     section_dict={'section_SW-NE': ([250, 250], [1750, 1750], [100, 100]),
-    #                   'section_NW-SE': ([250, 1750], [1750, 250], [100, 100])}
-    # )
+    gp.set_section_grid(
+        grid=geo_model.grid,
+        section_dict={'section_SW-NE': ([250, 250], [1750, 1750], [100, 100]),
+                      'section_NW-SE': ([250, 1750], [1750, 250], [100, 100])}
+    )
 
     gp.set_topography_from_random(
         grid=geo_model.grid,
         fractal_dimension=1.2,
-        d_z=np.array([600, 2000]),
+        d_z=np.array([200, 1000]),
         topography_resolution=np.array([60, 60])
     )
-
     gp.compute_model(geo_model)
 
-    plot_block_and_input_2d(
-        stack_number=0,
-        interpolation_input=geo_model.interpolation_input,
-        outputs=geo_model.solutions.octrees_output,
-        structure=geo_model.structural_frame.input_data_descriptor.stack_structure,
-        value_type=ValueType.ids
+    gpv.plot_2d(
+        model=geo_model,
+        section_names=['section_SW-NE', 'section_NW-SE', 'topography'],
+        direction=['x', 'y', 'y'], cell_number=['mid', 'mid', 'mid'],
+        show_lith=[False, False, False, True, True, True],
+        show_boundaries=[False, False, False, True, True, True],
+        show_scalar=[False, False, False, False, True, True],
+        series_n=[0, 0, 0, 0, 0, 0],
+        show_topography=True,
+        show_section_traces=True  # TODO: Test this one
     )
-    
-    gpv.plot_2d(geo_model, show_topography=True)
-    
+
+    gpv.plot_2d(geo_model, show_topography=True, show_boundaries=False, section_names=['topography'])
+    gpv.plot_2d(geo_model, show_boundaries=False, section_names=['topography'])
     gpv.plot_3d(geo_model, show_data=True, show_boundaries=True, show_lith=False, image=True)
