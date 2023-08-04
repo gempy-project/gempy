07d06aacb0ada901bef6de9af8567f5a2dc2d5f7
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-04 11:51:33 +0200

    - [ENH] Get topography plotting in pyvista done

--- gempy/modules/grids/create_topography.py
+++ gempy/modules/grids/create_topography.py
@@ -184,11 +184,12 @@ class LoadDEMGDAL:
 
 def create_random_topography(extent: np.array, resolution: np.array, dz: Optional[np.array] = None,
                              fractal_dimension: Optional[float] = 2.0) -> np.array:
- 
-    dem = _LoadDEMArtificial(extent=extent,
-                             resolution=resolution,
-                             d_z=dz,
-                             fd=fractal_dimension)
+    dem = _LoadDEMArtificial(
+        extent=extent,
+        resolution=resolution,
+        d_z=dz,
+        fd=fractal_dimension
+    )
 
     return dem.get_values()
 
