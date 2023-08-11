07c6c80650e21dc76d126c030100aaf3cafd91e4
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-11 15:39:23 +0200

    - [WIP] Getting ready for getting started

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -131,14 +131,14 @@ todo_include_todos = True  # If true, `todo` and `todoList` produce output, else
 sphinx_gallery_conf = {
     # path to your examples scripts
     "examples_dirs"          : [
-        # "../../examples/getting_started",
+        "../../examples/getting_started",
         "../../examples/tutorials",
         # "../../examples/examples",
         # "../../examples/integrations",
     ],
     # path where to save gallery generated examples
     "gallery_dirs"           : [
-        # 'getting_started',
+        'getting_started',
         'tutorials',
         # "examples",
         # 'integrations'
