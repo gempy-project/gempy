8344cf220f3a57ecb6976a52011d37d0bd7e188a
AlexanderJuestel <alexander.juestel@gmail.com>
2023-08-14 12:40:00 +0200

    AddArgument

--- gempy/plot/plot_api.py
+++ gempy/plot/plot_api.py
@@ -277,7 +277,9 @@ def plot_3d(model, plotter_type='basic',
             kwargs_plot_topography=None,
             kwargs_plot_data=None,
             image=False,
-            off_screen=False, **kwargs) -> GemPyToVista:
+            off_screen=False,
+            font_size: int =pv.global_theme.font.size,
+            **kwargs) -> GemPyToVista:
     """foobar
 
     Args:
@@ -297,6 +299,8 @@ def plot_3d(model, plotter_type='basic',
         ve (float): Vertical Exaggeration
         kwargs_plot_structured_grid:
         kwargs_plot_topography:
+        font_size : int
+            Font size for the labels of the grid
         **kwargs:
 
     Returns:
