2f6f3c1ba62413aec109fc95185f4ddac6f943b6
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-16 15:29:04 +0200

    - [CLN/DOC] Debugging faults offset and cleaning related code

--- examples/tutorials_/ch1_fundamentals/ch1_5_fault_relations.py
+++ examples/tutorials_/ch1_fundamentals/ch1_5_fault_relations.py
@@ -31,8 +31,8 @@ data_path = os.path.abspath('../../')
 geo_model: gp.data.GeoModel = gp.create_geomodel(
     project_name='Faults_relations',
     extent=[0, 1000, 0, 1000, -1000, -400],
-    resolution=[50, 50, 50],
-    number_octree_levels=5,
+    resolution=[20, 20, 20],
+    number_octree_levels=1,  # * For this model is better not to use octrees because we want to see what is happening in the scalar fields
     importer_helper=gp.data.ImporterHelper(
         path_to_orientations=data_path + "/data/input_data/tut-ch1-5/tut_ch1-5_orientations.csv",
         path_to_surface_points=data_path + "/data/input_data/tut-ch1-5/tut_ch1-5_points.csv",
@@ -50,7 +50,6 @@ print(geo_model)
 fault1: gp.data.StructuralElement = geo_model.structural_frame.get_element_by_name("fault1")
 fault2: gp.data.StructuralElement = geo_model.structural_frame.get_element_by_name("fault2")
 
-
 # Remove the faults from the default group
 default_group: gp.data.StructuralGroup = geo_model.structural_frame.get_group_by_name("default_formation")
 default_group.elements.remove(fault1)
@@ -66,22 +65,19 @@ gp.add_structural_group(
     fault_relations=gp.data.FaultsRelationSpecialCase.OFFSET_ALL
 )
 
-
 print(geo_model.structural_frame)
 
 # %% 
 
 geo_model.transform.apply_anisotropy(gp.data.GlobalAnisotropy.NONE)
-gp.compute_model(geo_model)
-
-# %% 
-
-print(geo_model.solutions.raw_arrays.block_matrix[0])  # This contains the block values for the fault1
-print(geo_model.solutions.raw_arrays.block_matrix[1])  # This contains the block values for the formations
-
-# %%
-gpv.plot_2d(geo_model, show_data=True)
-gpv.plot_3d(geo_model, show_data=True, kwargs_plot_structured_grid={'opacity': .2})
+if False:
+    gp.compute_model(geo_model)
+    # %%
+    print(geo_model.solutions.raw_arrays.block_matrix[0])  # This contains the block values for the fault1
+    print(geo_model.solutions.raw_arrays.block_matrix[1])  # This contains the block values for the formations
+    # %%
+    gpv.plot_2d(geo_model, show_data=True)
+    gpv.plot_3d(geo_model, show_data=True, kwargs_plot_structured_grid={'opacity': .2})
 
 # TODO: Add example of offsetting just one fault
 
@@ -97,32 +93,21 @@ gp.add_structural_group(
     structural_relation=gp.data.StackRelationType.FAULT,
     fault_relations=gp.data.FaultsRelationSpecialCase.OFFSET_ALL
 )
+print(geo_model.structural_frame)
 
+from gempy_engine.core.data.kernel_classes.solvers import Solvers
+geo_model.interpolation_options.kernel_options.kernel_solver = Solvers.SCIPY_CG
+geo_model.interpolation_options.kernel_options.compute_condition_number = True
 gp.compute_model(geo_model)
 
 # %%
 gpv.plot_2d(geo_model, show_data=True)
-gpv.plot_3d(geo_model, show_data=True, kwargs_plot_structured_grid={'opacity': .2})
-
-
-# %%
-# Offset parameter (Experimental)
-# -------------------------------
-#
+gpv.plot_3d(geo_model, show_data=True, image=True, kwargs_plot_structured_grid={'opacity': .2})
 
 # %%
-geo_model_graben._interpolator.aesara_graph.offset.set_value(1)
-gp.compute_model(geo_model_graben, compute_mesh=False)
-
-# %%
-gp.plot_2d(geo_model_graben, block=geo_model_graben.solutions.block_matrix[1, 0, :125000],
-           show_data=True)
-
-# %%
-gp.plot_2d(geo_model_graben, series_n=2, show_scalar=True)
-
-# %%
-geo_model_graben.solutions.scalar_field_matrix[1]
+gpv.plot_2d(geo_model, show_scalar=True, show_lith=False,series_n=0)
+gpv.plot_2d(geo_model, show_scalar=True, show_lith=False,series_n=1)
+gpv.plot_2d(geo_model, show_scalar=True, show_lith=False,series_n=2)
 
 # %%
 # Finding the faults intersection:
