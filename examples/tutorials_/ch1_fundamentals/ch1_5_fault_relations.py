c0bb84de2e81a2fa9ee71221398651c7974d9377
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-16 16:18:40 +0200

    - [DOC] Adding fault relations example

--- examples/tutorials_/ch1_fundamentals/ch1_5_fault_relations.py
+++ examples/tutorials_/ch1_fundamentals/ch1_5_fault_relations.py
@@ -32,7 +32,7 @@ geo_model: gp.data.GeoModel = gp.create_geomodel(
     project_name='Faults_relations',
     extent=[0, 1000, 0, 1000, -1000, -400],
     resolution=[20, 20, 20],
-    number_octree_levels=1,  # * For this model is better not to use octrees because we want to see what is happening in the scalar fields
+    number_octree_levels=6,  # * For this model is better not to use octrees because we want to see what is happening in the scalar fields
     importer_helper=gp.data.ImporterHelper(
         path_to_orientations=data_path + "/data/input_data/tut-ch1-5/tut_ch1-5_orientations.csv",
         path_to_surface_points=data_path + "/data/input_data/tut-ch1-5/tut_ch1-5_points.csv",
@@ -81,6 +81,7 @@ if False:
 
 # TODO: Add example of offsetting just one fault
 
+# %5
 # Graben example
 # --------------
 
@@ -96,6 +97,7 @@ gp.add_structural_group(
 print(geo_model.structural_frame)
 
 from gempy_engine.core.data.kernel_classes.solvers import Solvers
+
 geo_model.interpolation_options.kernel_options.kernel_solver = Solvers.SCIPY_CG
 geo_model.interpolation_options.kernel_options.compute_condition_number = True
 gp.compute_model(geo_model)
@@ -105,41 +107,52 @@ gpv.plot_2d(geo_model, show_data=True)
 gpv.plot_3d(geo_model, show_data=True, image=True, kwargs_plot_structured_grid={'opacity': .2})
 
 # %%
-gpv.plot_2d(geo_model, show_scalar=True, show_lith=False,series_n=0)
-gpv.plot_2d(geo_model, show_scalar=True, show_lith=False,series_n=1)
-gpv.plot_2d(geo_model, show_scalar=True, show_lith=False,series_n=2)
+gpv.plot_2d(geo_model, show_scalar=True, show_lith=False, series_n=0)
+gpv.plot_2d(geo_model, show_scalar=True, show_lith=False, series_n=1)
+gpv.plot_2d(geo_model, show_scalar=True, show_lith=False, series_n=2)
 
 # %%
-# Finding the faults intersection:
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# Sometimes we need to find the voxels that contain the each fault. To do
-# so we can use gempy's functionality to find interfaces as follows. Lets
-# use the first fault as an example:
-#
+# Finite Faults
+# -------------
+
+
 
 # %%
-gp.plot_2d(geo_model_graben,
-           regular_grid=geo_model_graben.solutions.block_matrix[0, 0, :125000],
-           show_data=True)
+# Faults relations
+# ----------------
 
 # %%
-# Remember the fault block is stored on:
-geo_model_graben.solutions.block_matrix[0, 0, :125000]
+# Let's split the formations in two groups
+
+gp.add_structural_group(
+    model=geo_model,
+    group_index=2,
+    structural_group_name="series_1",
+    elements=[
+        geo_model.structural_frame.get_element_by_name("rock4"),
+        geo_model.structural_frame.get_element_by_name("rock3")
+    ],
+    structural_relation=gp.data.StackRelationType.ERODE
+)
+
+default_group.elements.remove(geo_model.structural_frame.get_element_by_name("rock4"))
+default_group.elements.remove(geo_model.structural_frame.get_element_by_name("rock3"))
+
+gp.set_fault_relation(
+    frame=geo_model.structural_frame,
+    rel_matrix=np.array([
+        [0, 1, 1, 1],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+    )
+)
+print(geo_model.structural_frame)
 
 # %%
-# Now we can find where is the intersection of the values 1 by calling the following function.
-# This will return Trues on those voxels on the intersection
-intersection = find_interfaces_from_block_bottoms(
-    geo_model_graben.solutions.block_matrix[0, 0, :125000].reshape(50, 50, 50), 1, shift=1)
+gp.compute_model(geo_model)
 
 # %%
-# We can manually plotting together to see exactly what we have done
-ax = gp.plot_2d(geo_model_graben,
-                block=geo_model_graben.solutions.block_matrix[0, 0, :125000],
-                show_data=True, show=False)
-
-plt.imshow(intersection[:, 25, :].T, origin='lower', extent=(0, 1000, -1000, 0), alpha=.5)
-plt.show()
-
-gp.save_model(geo_model)
+gpv.plot_2d(geo_model, show_data=True)
+gpv.plot_3d(geo_model, show_data=True, image=False, kwargs_plot_structured_grid={'opacity': .2})
