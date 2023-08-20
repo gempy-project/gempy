03fded37ed059607b61fefebd52a4eef60472797
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-20 10:58:24 +0200

    - [DOC] Reviving 2d tutorial

--- examples/tutorials_/ch1_fundamentals/ch1_6_2d_visualization.py
+++ examples/tutorials_/ch1_fundamentals/ch1_6_2d_visualization.py
@@ -5,9 +5,10 @@
 
 # %%
 
+import os
+
 # Importing auxiliary libraries
 import numpy as np
-import os
 
 # Importing GemPy
 import gempy as gp
@@ -78,186 +79,7 @@ gp.compute_model(geo_data)
 # new plotting api
 gpv.plot_2d(geo_data, section_names=['section1'])
 
-# %%
-# or
 
-# %%
-gpv.plot_2d(geo_data, section_names=['section1'])
-
-# %%
-# Plot 2d: Object oriented:
-# -------------------------
-# 
-
-
-# %%
-# One plot
-# ^^^^^^^^
-# 
-
-# %% 
-p = gpv.plot_2d(geo_data, section_names=[], direction=None, show=False)
-p.fig.show()
-
-# %%
-p = gpv.plot_2d(geo_data, section_names=[], direction=None, show=False)
-# -----new code------
-
-sec_name = 'section1'
-s1 = p.add_section(sec_name)
-p.plot_data(s1, sec_name, projection_distance=200)
-p.fig.show()
-
-# %%
-p = gpv.plot_2d(geo_data, section_names=[], direction=None, show=False)
-sec_name = 'section1'
-s1 = p.add_section(sec_name)
-# -----new code------
-
-p.plot_data(s1, sec_name, projection_distance=200)
-p.plot_contacts(s1, sec_name)
-p.fig.show()
-
-# %%
-p = gpv.plot_2d(geo_data, section_names=[], direction=None, show=False)
-sec_name = 'section1'
-s1 = p.add_section(sec_name)
-p.plot_data(s1, sec_name, projection_distance=200)
-p.plot_contacts(s1, sec_name)
-# -----new code------
-
-p.plot_lith(s1, sec_name)
-p.plot_topography(s1, sec_name)
-p.fig.show()
-
-# %%
-# Several plots
-# ^^^^^^^^^^^^^
-# 
-
-# %% 
-
-sec_name = 'section1'
-sec_name_2 = 'section3'
-
-p2 = gpv.plot_2d(
-    geo_data,
-    n_axis=3,
-    figsize=(15, 15),  # General fig options
-    section_names=[sec_name, 'topography'],
-    cell_number=[3],  # Defining the sections
-    show_data=False,
-    show_lith=False,
-    show_scalar=False,
-    show_boundaries=False
-)
-
-# %% 
-
-# Create the section. This loacte the axes and give the right
-# aspect ratio and labels
-
-p2 = gpv.plot_2d(
-    geo_data,
-    n_axis=3,
-    figsize=(15, 15),  # General fig options
-    section_names=[sec_name, 'topography'],
-    cell_number=[3],  # Defining the sections
-    show_data=False,
-    show_lith=False,
-    show_scalar=False,
-    show_boundaries=False,
-    show=False
-)
-# -----new code------
-
-s1 = p2.add_section(sec_name_2, ax_pos=224)
-p2.fig.show()
-
-# %% 
-# Axes 0
-
-p2 = gpv.plot_2d(
-    geo_data, n_axis=3, figsize=(15, 15),  # General fig options
-    section_names=[sec_name, 'topography'], cell_number=[3],  # Defining the sections
-    show_data=False, show_lith=False, show_scalar=False, show_boundaries=False,
-    show=False)
-s1 = p2.add_section(sec_name_2, ax_pos=224)
-# -----new code------
-
-p2.plot_contacts(s1, sec_name_2)
-p2.plot_lith(s1, sec_name_2)
-p2.plot_data(s1, sec_name_2, projection_distance=200)
-p2.plot_topography(s1, sec_name_2)
-p2.fig.show()
-
-# %%
-# Axes 1
-
-# sphinx_gallery_thumbnail_number = 12
-p2 = gpv.plot_2d(
-    geo_data,
-    n_axis=3,
-    figsize=(15, 15),  # General fig options
-    section_names=[sec_name, 'topography'],
-    cell_number=[3],  # Defining the sections
-    show_data=False,
-    show_lith=False,
-    show_scalar=False,
-    show_boundaries=False,
-    show=False
-)
-s1 = p2.add_section(sec_name_2, ax_pos=224)
-p2.plot_contacts(s1, sec_name_2)
-p2.plot_lith(s1, sec_name_2)
-p2.plot_data(s1, sec_name_2, projection_distance=200)
-p2.plot_topography(s1, sec_name_2)
-# -----new code------
-
-p2.plot_contacts(p2.axes[0], cell_number=3)
-p2.plot_scalar_field(p2.axes[0], cell_number=3, series_n=1)
-p2.fig.show()
-
-# %%
-# Axes2
-
-p2 = gpv.plot_2d(
-    geo_data,
-    n_axis=3,
-    figsize=(15, 15),  # General fig options
-    section_names=[sec_name, 'topography'],
-    cell_number=[3],  # Defining the sections
-    show_data=False,
-    show_lith=False,
-    show_scalar=False,
-    show_boundaries=False,
-    show=False
-)
-
-s1 = p2.add_section(sec_name_2, ax_pos=224)
-p2.plot_contacts(s1, sec_name_2)
-p2.plot_lith(s1, sec_name_2)
-p2.plot_data(s1, sec_name_2, projection_distance=200)
-p2.plot_topography(s1, sec_name_2)
-p2.plot_contacts(p2.axes[0], cell_number=3)
-p2.plot_scalar_field(p2.axes[0], cell_number=3, series_n=1)
-# -----new code------
-
-p2.plot_lith(p2.axes[1], 'topography')
-p2.plot_contacts(p2.axes[1], 'topography')
-p2.fig.show()
-
-# %%
-# Plotting traces:
-# ''''''''''''''''
-# 
-
-# %% 
-p2.plot_section_traces(p2.axes[1])
-p2.fig.show()
-
-# %% 
-gpv.plot_section_traces(geo_data)
 
 # %%
 # Plot API
@@ -288,4 +110,5 @@ gpv.plot_2d(geo_data, section_names=['section1'])
 gpv.plot_2d(geo_data, section_names=['section1', 'section2'])
 
 # %% 
-gpv.plot_2d(geo_data, figsize=(15, 15), section_names=['section1', 'section2', 'topography'], cell_number='mid')
+gpv.plot_2d(
+    geo_data, figsize=(15, 15), section_names=['section1', 'section2', 'topography'], cell_number='mid')
