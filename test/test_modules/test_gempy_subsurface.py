31b15787a50fc640063da28c917b70a18566b531
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-12 14:14:20 +0200

    - [WIP] Starting to revive gempy to subsurface code

---
+++ test/test_modules/test_gempy_subsurface.py
@@ -0,0 +1,7 @@
+﻿import gempy as gp
+from gempy.core.data.enumerators import ExampleModel
+
+
+def test_gempy_to_subsurface():
+    model = gp.generate_example_model(ExampleModel.ANTICLINE, compute_model=True)
+    
