b37602d8b74dfe5ecd480127ee1b77f08f7632a9
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-11-22 16:33:26 +0100

    [META] Tweak requirements to release gempy 2023.2

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
     packages=find_packages(exclude=('test', 'docs', 'examples')),
     install_requires=read_requirements("requirements/requirements.txt"),
     extras_require={
-        "dev": read_requirements("requirements/dev-requirements.txt"),
         "opt": read_requirements("requirements/optional-requirements.txt"),
         "base": read_requirements("requirements/base-requirements.txt"),
     },
