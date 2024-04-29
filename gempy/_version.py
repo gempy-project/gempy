74c1208d578230918520fc82b5ceb425c86dd69d
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-29 11:21:47 +0200

    [ENH] Add new _version.py file
    A new _version.py file has been added to handle the versioning of the project. This file was generated by setuptools_scm and contains different variables regarding version, version tuple, etc.

--- gempy/_version.py
+++ gempy/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '2024.1.3.dev0+g0ca36d5d.d20240429'
-__version_tuple__ = version_tuple = (2024, 1, 3, 'dev0', 'g0ca36d5d.d20240429')
+__version__ = version = '2024.1.2'
+__version_tuple__ = version_tuple = (2024, 1, 2)
