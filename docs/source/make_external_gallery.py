dcff49e1d077233b4d1b012ab2344a2db92a89e3
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-06-13 18:30:43 +0200

    [DOC] Update examples in external gallery script
    Modified the 'docs/source/make_external_gallery.py' script to include additional examples and updated the description for existing ones. The changes also include renaming some dictionary keys for consistency and clarity.

--- docs/source/make_external_gallery.py
+++ docs/source/make_external_gallery.py
@@ -34,18 +34,30 @@ class Example():
 ###############################################################################
 
 articles = dict(
-    advanced_bayes=Example(
+    _1=Example(
         title="Vector Model 1: Construction",
         description="Build a basic structural model",
         link="https://vector-raw-materials.github.io/vector-geology/examples/02_structural_modeling/01_model_1_gempy_step_by_step.html#sphx-glr-examples-02-structural-modeling-01-model-1-gempy-step-by-step-py",
         image="https://vector-raw-materials.github.io/vector-geology/_images/sphx_glr_01_model_1_gempy_step_by_step_001.png",
     ),
-    simple_bayes=Example(
+    _2=Example(
         title="Vector Model 1: Forward Gravity",
-        description="Build a basic Bayesian model with Pyro",
+        description="Get the gravity response of the model",
         link="https://vector-raw-materials.github.io/vector-geology/examples/03_forward_engines/02_model_1_gempy_fw_gravity.html#sphx-glr-examples-03-forward-engines-02-model-1-gempy-fw-gravity-py",
         image="https://vector-raw-materials.github.io/vector-geology/_images/sphx_glr_02_model_1_gempy_fw_gravity_003.png",
-    )
+    ),
+    _3=Example(
+        title="Spremberg: Importing Boreholes",
+        description="Import data from well csv files",
+        link="https://vector-raw-materials.github.io/vector-geology/examples/02_structural_modeling/03_model_spremberg_import.html#sphx-glr-examples-02-structural-modeling-03-model-spremberg-import-py",
+        image="https://vector-raw-materials.github.io/vector-geology/_images/sphx_glr_03_model_spremberg_import_003.png",
+    ),
+    _4=Example(
+        title="Spremberg: Constructing the Model",
+        description="Build a structural model from borehole data",
+        link="https://vector-raw-materials.github.io/vector-geology/examples/02_structural_modeling/04_model_spremberg_building.html#sphx-glr-examples-02-structural-modeling-04-model-spremberg-building-py",
+        image="https://vector-raw-materials.github.io/vector-geology/_images/sphx_glr_04_model_spremberg_building_008.png",
+    ),
 )
 
 
