5f2409d4817cd75506ee4f441cb5237102180403
MigueldelaVarga <miguel@terranigma-solutions.com>
2025-03-15 15:14:43 +0000

    [TEST] Trying to fix las 2 tests in ci

--- test/test_modules/test_gempy_subsurface.py
+++ test/test_modules/test_gempy_subsurface.py
@@ -36,7 +36,8 @@ def test_gempy_to_subsurface():
 
     concatenated_id_array = np.concatenate(vertex_id_array)
     concatenated_cell_id_array = np.concatenate(cell_id_array)
-    
+
+    ss = optional_dependencies.require_subsurface()
     meshes: ss.UnstructuredData = ss.UnstructuredData.from_array(
         vertex=np.concatenate(vertex),
         cells=np.concatenate(simplex_list),
