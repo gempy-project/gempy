04f435592ffa7c0656e206392241eb105eebacb2
MigueldelaVarga <miguel@terranigma-solutions.com>
2025-06-11 09:13:27 +0200

    [TEST] Fixing tests that return stuff (breaking changing from pytest)

--- test/test_api/test_model_construction_granular.py
+++ test/test_api/test_model_construction_granular.py
@@ -23,46 +23,51 @@ from gempy.optional_dependencies import require_gempy_viewer
 """
 
 def test_read_input_points():
+    _, _ = _read_data()
+
+
+def _read_data() -> tuple[OrientationsTable, SurfacePointsTable]:
     data_path = 'https://raw.githubusercontent.com/cgre-aachen/gempy_data/master/'
     surface_points_file = pooch.retrieve(
         url=data_path + "data/input_data/jan_models/model1_surface_points.csv",
         known_hash="6f1a39ed77e87a4057f03629c946b1876b87e24409cadfe0e1cf7ab1488f69e4"
     )
-
     orientations_file = pooch.retrieve(
         url=data_path + "data/input_data/jan_models/model1_orientations.csv",
         known_hash="04c307ae23f70252fe54144a2fb95ca7d96584a2d497ea539ed32dfd23e7cd5d"
     )
-
     print(pooch.file_hash(surface_points_file))
     print(pooch.file_hash(orientations_file))
-
     surface_points: SurfacePointsTable = read_surface_points(
         path=surface_points_file,
     )
-
     orientations: OrientationsTable = read_orientations(
         path=orientations_file
     )
+    return orientations, surface_points
 
-    return surface_points, orientations
+
+def test_create_grid():
+    _generate_grid()
 
 
-def test_create_grid() -> gp.data.Grid:
+def _generate_grid():
     grid: gp.data.Grid = gp.data.Grid(
         extent=[0, 1000, 0, 1000, 0, 500],
         resolution=[50, 5, 50]
     )
-
     return grid
 
 
-def test_create_structural_frame() -> StructuralFrame:
+def test_create_structural_frame():
     # * Structural elements
-    surface_points, orientations = test_read_input_points()
+    _create_structural_frame()
+
+
+def _create_structural_frame():
+    surface_points, orientations = _read_data()
     surface_points_groups = surface_points.get_surface_points_by_id_groups()
     orientations_groups = orientations.get_orientations_by_id_groups()
-
     structural_elements = []
     color_gen = ColorsGenerator()
     for i in range(len(surface_points_groups)):
@@ -75,46 +80,46 @@ def test_create_structural_frame() -> StructuralFrame:
         )
 
         structural_elements.append(structural_element)
-
     # * Structural groups definitions
     default_formation: Stack = Stack(
         name="default_formation",
         elements=structural_elements,
         structural_relation=gp.data.StackRelationType.ERODE
     )
-
     # ? Should I move this to the constructor?
     structural_frame: StructuralFrame = StructuralFrame(
         structural_groups=[default_formation],
         color_gen=color_gen
     )
-
     return structural_frame
 
 
-def test_create_interpolation_options() -> InterpolationOptions:
+def test_create_interpolation_options():
+    interpolation_options = _generate_interpolation_options()
+
+
+def _generate_interpolation_options():
     range_ = 1000.0
     interpolation_options: InterpolationOptions = InterpolationOptions.from_args(
         range=range_,
         c_o=(range_ ** 2) / 14 / 3,
     )
-
     return interpolation_options
 
 
 def test_create_geomodel() -> GeoModel:
     geo_model: GeoModel = GeoModel.from_args(
         name="horizontal",
-        structural_frame=test_create_structural_frame(),
-        grid=test_create_grid(),
-        interpolation_options=test_create_interpolation_options()
+        structural_frame=_create_structural_frame(),
+        grid=_generate_grid(),
+        interpolation_options=_generate_interpolation_options()
     )
 
     return geo_model
 
 
 def test_structural_frame_surface_points():
-    structural_frame: StructuralFrame = test_create_structural_frame()
+    structural_frame: StructuralFrame = _create_structural_frame()
     print(structural_frame.surface_points_copy)
     pass
 
