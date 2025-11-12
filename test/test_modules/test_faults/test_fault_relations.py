56dac8f0f2d534f2a72c802f1e618fcd63cf942e
NilsChudalla <62913082+NilsChudalla@users.noreply.github.com>
2025-11-12 16:32:43 +0100

    Tests to check if set fault relations work. Example Model "FAULT_RELATION" is based on the model from tutorial ch1_5, where this did not work. For the video tutorial model, set fault relations does work.

---
+++ test/test_modules/test_faults/test_fault_relations.py
@@ -0,0 +1,32 @@
+import numpy as np
+import gempy as gp
+from gempy.core.data.enumerators import ExampleModel
+from gempy.optional_dependencies import require_gempy_viewer
+
+PLOT = True
+
+
+def test_fault_relations_implementation():
+    model = gp.generate_example_model(ExampleModel.FAULT_RELATION, compute_model=True)
+
+    correct_relations = np.array([
+        [0, 1, 1, 1],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ], dtype=bool)
+
+    # Assert
+    assert np.array_equal(model.structural_frame.fault_relations, correct_relations) == True
+
+    if PLOT:
+        gpv = require_gempy_viewer()
+        gtv: gpv.GemPyToVista = gpv.plot_3d(
+            model=model,
+            show_data=True,
+            image=True,
+            show=True
+        )
+
+if __name__ == "__main__":
+    test_fault_relations_implementation()  
