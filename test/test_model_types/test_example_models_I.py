0c21b440dc02810d3b472d2be7d9e47700d215e0
Miguel de la Varga <miguel@terranigma-solutions.com>
2026-01-22 14:58:38 +0100

    [TEST] Add 3D plotting options to combination model tests and improve type hinting
    Enhanced the combination model tests with additional 3D plotting options (e.g., toggling octree display). Improved code readability with added type hints for better clarity and maintainability.

--- test/test_model_types/test_example_models_I.py
+++ test/test_model_types/test_example_models_I.py
@@ -1,11 +1,18 @@
-﻿import gempy as gp
+﻿from typing import Any
+
+from numpy import dtype, ndarray
+
+import gempy as gp
+from gempy.core.data import GeoModel
 from gempy.core.data.enumerators import ExampleModel
 from gempy.optional_dependencies import require_gempy_viewer
 from gempy_engine.core.data.interp_output import InterpOutput
+from gempy_viewer import GemPyToVista
 
 from test.verify_helper import gempy_verify_array
 import pytest
 from test.conftest import TEST_SPEED, TestSpeed
+
 # ! When importing the model is computed
 
 # TODO []: Use the generator and do some approval testing
@@ -17,9 +24,8 @@ pytestmark = pytest.mark.skipif(TEST_SPEED.value < TestSpeed.MINUTES.value, reas
 def _verify_scalar_field(model, name):
     outputs_centers_: InterpOutput = model.solutions.octrees_output[-1].outputs_centers[0]
     scalar_field = outputs_centers_.exported_fields.scalar_field
-    scalar_field = scalar_field[::int(len(scalar_field) / 50)] # Pick 50 values from the scalar field array
+    scalar_field = scalar_field[::int(len(scalar_field) / 50)]  # Pick 50 values from the scalar field array
     gempy_verify_array(scalar_field, name)
-
 
 
 def test_generate_horizontal_stratigraphic_model():
@@ -29,7 +35,7 @@ def test_generate_horizontal_stratigraphic_model():
     if PLOT:
         gpv = require_gempy_viewer()
         gpv.plot_3d(model, show_data=True, image=True)
-        
+
     if False:
         _verify_scalar_field(
             model=model,
@@ -49,7 +55,6 @@ def test_generate_fold_model():
         model=model,
         name="Anticline Scalar Field"
     )
-    
 
 
 def test_generate_fault_model():
@@ -64,18 +69,25 @@ def test_generate_fault_model():
         model=model,
         name="Fault Scalar Field"
     )
-    
 
 
 def test_generate_combination_model():
+
     model = gp.generate_example_model(ExampleModel.COMBINATION, compute_model=True)
     print(model.structural_frame)
 
     if PLOT:
         gpv = require_gempy_viewer()
-        gpv.plot_3d(model, image=True)
-        
-    
+        pd3 = gpv.plot_3d(
+            model,
+            image=False,
+            show_lith=False,
+            show_octree=True,
+            show=True
+        )
+
+    return
+
     _verify_scalar_field(
         model=model,
         name="Combination Scalar Field"
@@ -89,4 +101,3 @@ def test_generate_greenstone_model():
     if PLOT:
         gpv = require_gempy_viewer()
         gpv.plot_3d(model, image=True)
-
